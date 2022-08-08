7f9696096e11a1eb2c74cd983e7b1d79481132eb
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-09 00:11:17 +0200

    Add PrefixSaver

---
+++ src/main/java/dev/l3g7/griefer_utils/features/features/PrefixSaver.java
@@ -0,0 +1,73 @@
+package dev.l3g7.griefer_utils.features.features;
+
+import dev.l3g7.griefer_utils.event.event_bus.EventListener;
+import dev.l3g7.griefer_utils.event.events.chat.MessageSendEvent;
+import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.file_provider.Singleton;
+import dev.l3g7.griefer_utils.misc.GuiPrefixSaver;
+import dev.l3g7.griefer_utils.misc.TickScheduler;
+import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Material;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Singleton
+public class PrefixSaver extends Feature {
+
+	private final BooleanSetting enabled = new BooleanSetting()
+	 .name("PrefixSaver")
+	 .config("features.prefix_saver.active")
+	 .icon(Material.COMPASS)
+	 .defaultValue(false);
+
+	public PrefixSaver() {
+		super(Category.FEATURE);
+	}
+
+	@Override
+	public SettingsElement getMainElement() {
+		return enabled;
+	}
+
+	@SubscribeEvent
+	public void onMouseClick(MouseEvent event) {
+		if (!isActive())// || !isOnGrieferGames())
+			return;
+
+		if (event.button != 1 || !event.buttonstate)
+			return;
+
+		if (!isHoldingPrefix())
+			return;
+
+		TickScheduler.runNextTick(() -> {
+			mc().displayGuiScreen(new GuiPrefixSaver(mc()));
+		});
+		event.setCanceled(true);
+
+	}
+
+	private boolean isHoldingPrefix() {
+		ItemStack itemStack = mc().thePlayer.getHeldItem();
+		if (itemStack == null)
+			return false;
+
+		NBTTagCompound tag = itemStack.getTagCompound();
+		if (tag == null || !tag.hasKey("display", 10))
+			return false;
+
+		NBTTagCompound nbt = tag.getCompoundTag("display");
+		if (nbt.getTagId("Lore") != 9)
+			return false;
+
+		NBTTagList lore = nbt.getTagList("Lore", 8);
+		if (lore.tagCount() < 1)
+			return false;
+
+		return lore.getStringTagAt(lore.tagCount() - 1).equals("\u00a7fVergibt \u00a7aein Farbrecht\u00a7f! (Rechtsklick)");
+	}
+}
---
+++ src/main/java/dev/l3g7/griefer_utils/misc/GuiPrefixSaver.java
@@ -0,0 +1,54 @@
+package dev.l3g7.griefer_utils.misc;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+public class GuiPrefixSaver extends GuiChest {
+
+	private static final int ACCEPT_SLOT_ID = 12;
+	private static final int DECLINE_SLOT_ID = 14;
+	public static final IInventory inv = new InventoryBasic("§6Möchtest du den Prefix benutzen?", false, 27);
+
+	public GuiPrefixSaver(Minecraft mc) {
+		super(mc.thePlayer.inventory, inv);
+	}
+
+	@Override
+	protected void handleMouseClick(Slot slotIn, int slotId, int clickedButton, int clickType) {
+		if (slotIn != null)
+			slotId = slotIn.slotNumber;
+
+		if (slotId == DECLINE_SLOT_ID)
+			mc.thePlayer.closeScreenAndDropStack();
+
+		if (slotId != ACCEPT_SLOT_ID)
+			return;
+
+		mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.getHeldItem());
+		mc.displayGuiScreen(null);
+	}
+
+	static {
+		ItemStack pane = new ItemStack(Blocks.stained_glass_pane);
+		pane.setItemDamage(7);
+		for (int i = 0; i < 27; i++)
+			inv.setInventorySlotContents(i, pane);
+
+		ItemStack accept = new ItemStack(Items.dye);
+		accept.setItemDamage(10); // Hellgrüne Farbe
+		accept.setStackDisplayName("§aBestätigen");
+		inv.setInventorySlotContents(ACCEPT_SLOT_ID, accept);
+
+
+		ItemStack decline = new ItemStack(Items.dye);
+		decline.setItemDamage(1); // Rote Farbe
+		decline.setStackDisplayName("§cAbbrechen");
+		inv.setInventorySlotContents(DECLINE_SLOT_ID, decline);
+	}
+}

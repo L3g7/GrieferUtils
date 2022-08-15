ae983d9342118eea54ec6318bcf1cdc32119d0e7
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-15 23:38:57 +0200

    Fix ToolSaver not working when breaking multiple blocks at once

--- src/main/java/dev/l3g7/griefer_utils/features/features/ToolSaver.java
+++ src/main/java/dev/l3g7/griefer_utils/features/features/ToolSaver.java
@@ -4,9 +4,11 @@ import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.elements.NumberSetting;
 import net.labymod.settings.elements.SettingsElement;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Singleton
 public class ToolSaver extends Feature {
@@ -45,5 +47,26 @@ public class ToolSaver extends Feature {
 
         if (damage.get() >= heldItem.getMaxDamage() - heldItem.getItemDamage())
             event.setCanceled(true);
+    }
+
+    // Required because when you break multiple blocks at once, the MouseEvent
+    // is only triggered once, but the held item can be damaged multiple times
+    @SubscribeEvent
+    public void onTick(TickEvent event) {
+        if (damage.get() == 0 || !isCategoryEnabled())
+            return;
+
+        if (player() == null)
+            return;
+
+        ItemStack heldItem = player().getHeldItem();
+        if (heldItem == null || heldItem.getMaxDamage() == 0)
+            return;
+
+        if (damage.get() < heldItem.getMaxDamage() - heldItem.getItemDamage())
+            return;
+
+        KeyBinding.setKeyBindState(mc().gameSettings.keyBindUseItem.getKeyCode(), false);
+        KeyBinding.setKeyBindState(mc().gameSettings.keyBindAttack.getKeyCode(), false);
     }
 }

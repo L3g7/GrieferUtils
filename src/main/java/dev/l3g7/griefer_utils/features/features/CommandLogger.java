18641db27b059097a01ee29ac447d26335d9b86f
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-16 16:24:59 +0200

    Added CommandLogger

---
+++ src/main/java/dev/l3g7/griefer_utils/features/features/CommandLogger.java
@@ -0,0 +1,35 @@
+package dev.l3g7.griefer_utils.features.features;
+
+import dev.l3g7.griefer_utils.event.event_bus.EventListener;
+import dev.l3g7.griefer_utils.event.events.chat.MessageSendEvent;
+import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.file_provider.Singleton;
+import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Material;
+
+@Singleton
+public class CommandLogger extends Feature {
+
+	private final BooleanSetting enabled = new BooleanSetting()
+			.name("CommandLogger")
+			.description("Schreibt die Befehle, die du ausführst in die Konsole und damit den Log.")
+			.config("features.command_logger.active")
+			.icon(Material.COMMAND)
+			.defaultValue(false);
+
+	@Override
+	public SettingsElement getMainElement() {
+		return enabled;
+	}
+
+	public CommandLogger() {
+		super(Category.FEATURE);
+	}
+
+	@EventListener
+	public void onMessageSend(MessageSendEvent event) {
+		if (isActive() && isOnGrieferGames() && event.getMsg().startsWith("/"))
+			System.out.println("Executed command: " + event.getMsg());
+	}
+}

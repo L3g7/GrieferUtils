40a7edb687660ce9084b4da78da27448f641f9e7
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-18 23:06:39 +0200

    Regex Bugfix Nr. 3

--- src/main/java/dev/l3g7/griefer_utils/misc/Constants.java
+++ src/main/java/dev/l3g7/griefer_utils/misc/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
     public static final Pattern FORMATTED_RANK_PATTERN = Pattern.compile("(?<rank>[§\\w+]{3,})");
     public static final Pattern FORMATTED_DELIMITER_PATTERN = Pattern.compile("§r§8§*l*\\u2503");
     public static final Pattern FORMATTED_JAVA_PLAYER_NAME_PATTERN = Pattern.compile("[~§\\w]{3,}");
-    public static final Pattern FORMATTED_BEDROCK_PLAYER_NAME_PATTERN = Pattern.compile("![§\\w+]{3,}");
+    public static final Pattern FORMATTED_BEDROCK_PLAYER_NAME_PATTERN = Pattern.compile("[!§\\w+]{3,}");
     public static final Pattern FORMATTED_PLAYER_NAME_PATTERN = Pattern.compile(String.format("(?<name>%s|%s)", FORMATTED_JAVA_PLAYER_NAME_PATTERN, FORMATTED_BEDROCK_PLAYER_NAME_PATTERN));
     public static final Pattern FORMATTED_PLAYER_PATTERN = Pattern.compile(String.format("(?<player>%s %s %s)", FORMATTED_RANK_PATTERN, FORMATTED_DELIMITER_PATTERN, FORMATTED_PLAYER_NAME_PATTERN));
 

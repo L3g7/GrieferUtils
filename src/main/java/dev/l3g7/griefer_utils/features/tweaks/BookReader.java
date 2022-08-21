77b8b3d737c93428ff17a5e3600c1d7f8016e2cc
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-21 18:22:41 +0200

    Moved BookReader to tweaks

--- src/main/java/dev/l3g7/griefer_utils/features/tweaks/BookReader.java
+++ src/main/java/dev/l3g7/griefer_utils/features/tweaks/BookReader.java
@@ -1,4 +1,4 @@
-package dev.l3g7.griefer_utils.features.features;
+package dev.l3g7.griefer_utils.features.tweaks;
 
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.file_provider.Singleton;

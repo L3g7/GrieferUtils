c55f5de51586a1a375507bdfcf10ac0044887aca
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-08-24 01:29:13 +0200

    Updated BetterSwitchCmd to the new system

--- src/main/java/dev/l3g7/griefer_utils/features/tweaks/BetterSwitchCmd.java
+++ src/main/java/dev/l3g7/griefer_utils/features/tweaks/BetterSwitchCmd.java
@@ -1,5 +1,6 @@
 package dev.l3g7.griefer_utils.features.tweaks;
 
+import com.google.common.collect.ImmutableMap;
 import dev.l3g7.griefer_utils.event.event_bus.EventListener;
 import dev.l3g7.griefer_utils.event.events.chat.MessageSendEvent;
 import dev.l3g7.griefer_utils.event.events.server.CityBuildJoinEvent;
@@ -7,34 +8,38 @@ import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
 import dev.l3g7.griefer_utils.misc.Constants;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
-import dev.l3g7.griefer_utils.util.Reflection;
 import net.labymod.settings.elements.SettingsElement;
 import net.labymod.utils.Material;
-import net.minecraft.client.gui.inventory.GuiChest;
-import net.minecraft.init.Blocks;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.Map;
 
 @Singleton
+@SuppressWarnings("unchecked") // Java doesn't like the generics of the map builder, so i have to cast it -> unchecked
 public class BetterSwitchCmd extends Feature {
 
 	private static final String ALL = getRegex("\\S+");
-	private static final String[] SPECIAL_SERVER = new String[] {"n|nature", "x|extreme", "e|evil", "w|wasser", "l|lava", "v|event"};
+	private static final Map<String, String> SPECIAL_SERVERS = (ImmutableMap<String, String>) (Object) ImmutableMap.builder()
+			.put("n|nature", "nature")
+			.put("x|extreme", "extreme")
+			.put("e|evil", "cbevil")
+			.put("w|wasser", "farm1")
+			.put("l|lava", "nether1")
+			.put("v|event", "eventserver")
+			.build();
 	private static final String NUMBER = getRegex("\\d+");
 
 	private static String getRegex(String cityBuild) {
-		return "^/(?:cb ?|switch )(" + cityBuild + ")(?: (.*))?$";
+		return "(?i)^/(?:cb ?|switch )(" + cityBuild + ")(?: (.*))?$";
 	}
 
-	private Integer slot = null;
 	private String command = "";
+	private boolean joined = false;
 
 	private final BooleanSetting enabled = new BooleanSetting()
 			.name("BetterSwitchCmd")
 			.config("tweaks.better_switch_cmd.active")
-			.description("Ermöglicht das direkte Joinen auf einen Citybuild. Siehe '/cb'.")
+			.description("Fügt Aliasse für /switch <cb> hinzu (siehe '/cb').\n" +
+					"Der Text nach dem Citybuild wird beim Beitritt in den Chat geschrieben.")
 			.icon(Material.COMPASS)
 			.defaultValue(true);
 
@@ -52,10 +57,10 @@ public class BetterSwitchCmd extends Feature {
 		if (!isActive() || !isOnGrieferGames())
 			return;
 
-		String msg = event.getMsg().toLowerCase();
+		String msg = event.getMsg();
 
 		if (msg.equals("/cb")) {
-			display(Constants.ADDON_PREFIX + "Syntax: '/switch <CB>', '/cb <CB>' oder '/cb<CB>'.");
+			display(Constants.ADDON_PREFIX + "Syntax: '/switch <CB> [command]', '/cb <CB> [command]' oder '/cb<CB> [command]'.");
 			display(Constants.ADDON_PREFIX + "§7§nShortcuts:");
 			display(Constants.ADDON_PREFIX + "§7Nature: 'n'");
 			display(Constants.ADDON_PREFIX + "§7Extreme: 'x'");
@@ -67,13 +72,13 @@ public class BetterSwitchCmd extends Feature {
 			return;
 		}
 
-		for (int i = 0; i < SPECIAL_SERVER.length; i++)
-			if (msg.matches(getRegex(SPECIAL_SERVER[i])))
-				join(i + 23);
+		for (String s : SPECIAL_SERVERS.keySet())
+			if (!joined && msg.matches(getRegex(s)))
+				join(SPECIAL_SERVERS.get(s));
 
-		if (slot == null && msg.matches(NUMBER))
-			join(Integer.parseInt(msg.replaceAll(NUMBER, "$1")));
-		else if (slot == null)
+		if (!joined && msg.matches(NUMBER))
+			join("cb" + msg.replaceAll(NUMBER, "$1"));
+		else if (!joined)
 			return;
 
 
@@ -81,34 +86,9 @@ public class BetterSwitchCmd extends Feature {
 		event.setCanceled(true);
 	}
 
-	@SubscribeEvent
-	public void onTick(TickEvent ignored) {
-		if (slot == null || !(mc().currentScreen instanceof GuiChest))
-			return;
-
-		IInventory inv = Reflection.get(mc().currentScreen, "lowerChestInventory", "field_147015_w", "w");
-		if (!inv.getDisplayName().getUnformattedText().equals("§6§lServerwechsel")
-				|| inv.getStackInSlot(10) == null
-				|| !inv.getStackInSlot(10).getItem().equals(Item.getItemFromBlock(Blocks.diamond_block)))
-			return;
-
-		mc().playerController.windowClick(player().openContainer.windowId, slot, 0, 0, player());
-		slot = null;
-	}
-
-	public void join(int cb) {
-		if (cb < 1 || cb > 28) {
-			display(Constants.ADDON_PREFIX + "Citybuild %d existiert nicht!", cb);
-			return;
-		}
-
-		cb--;
-
-		int row = cb / 7;
-		int column = cb % 7;
-		slot = (row * 9 + column) + 10;
-
-		send("/switch");
+	public void join(String cb) {
+		joined = true;
+		send("/switch " + cb);
 	}
 
 	@EventListener
@@ -118,6 +98,7 @@ public class BetterSwitchCmd extends Feature {
 
 		send(command);
 		command = "";
+		joined = false;
 	}
 
 }

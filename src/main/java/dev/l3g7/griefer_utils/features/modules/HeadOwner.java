3b6f86fa2414fff5831ce4a9c738ee05db80209d
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-09-30 18:49:39 +0200

    Made HeadOwner also trigger on armor stands

--- src/main/java/dev/l3g7/griefer_utils/features/modules/HeadOwner.java
+++ src/main/java/dev/l3g7/griefer_utils/features/modules/HeadOwner.java
@@ -4,9 +4,18 @@ package dev.l3g7.griefer_utils.features.modules;
 import dev.l3g7.griefer_utils.features.Module;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
 import net.labymod.settings.elements.ControlElement.IconData;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.item.ItemSkull;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntitySkull;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+
+import java.util.List;
 
 @Singleton
 public class HeadOwner extends Module {
@@ -27,13 +36,40 @@ public class HeadOwner extends Module {
             TileEntitySkull s = (TileEntitySkull) e;
             if (s.getPlayerProfile() != null && s.getPlayerProfile().getName() != null && !s.getPlayerProfile().getName().isEmpty())
                 return new String[]{s.getPlayerProfile().getName()};
+			return getDefaultValues();
         }
-        return getDefaultValues();
+
+		EntityArmorStand armorStand = rayTraceArmorStand();
+
+		// Source: ItemSkull.getItemStackDisplayName(ItemStack stack)
+	    if (armorStand == null)
+			return getDefaultValues();
+
+	    ItemStack item = armorStand.getCurrentArmor(3);
+		if (item == null || !(item.getItem() instanceof ItemSkull))
+			return getDefaultValues();
+
+		if (item.getMetadata() != 3 || !item.hasTagCompound())
+			return getDefaultValues();
+
+	    NBTTagCompound tag = item.getTagCompound();
+
+	    if (tag.hasKey("SkullOwner", 8))
+		    return new String[]{tag.getString("SkullOwner")};
+
+	    if (tag.hasKey("SkullOwner", 10)) {
+		    NBTTagCompound nbttagcompound = tag.getCompoundTag("SkullOwner");
+
+		    if (nbttagcompound.hasKey("Name", 8))
+			    return new String[]{nbttagcompound.getString("Name")};
+	    }
+
+	    return getDefaultValues();
     }
 
     @Override
     public boolean isShown() {
-        return super.isShown() && rayTraceTileEntity() instanceof TileEntitySkull;
+        return super.isShown() && (rayTraceTileEntity() instanceof TileEntitySkull || rayTraceArmorStand() != null);
     }
 
     private TileEntity rayTraceTileEntity() {
@@ -47,5 +83,50 @@ public class HeadOwner extends Module {
         return mc.theWorld.getTileEntity(rayTraceResult.getBlockPos());
     }
 
+	/**
+	 * Source: EntityRenderer.getMouseOver(float partialTicks)
+	 */
+	private EntityArmorStand rayTraceArmorStand() {
+
+		Entity pointedEntity = null;
+
+		double distance = 1000;
+
+		Entity renderEntity = mc.getRenderViewEntity();
+
+		Vec3 look = renderEntity.getLook(1);
+		Vec3 eyes = renderEntity.getPositionEyes(1);
+		Vec3 maxEyes = eyes.addVector(look.xCoord * 1000, look.yCoord * 1000, look.zCoord * 1000);
+
+		List<Entity> loadedEntities = mc.theWorld.loadedEntityList;
+
+		for (Entity entity : loadedEntities) {
+			if (!(entity instanceof EntityArmorStand))
+				continue;
+
+			float borderSize = entity.getCollisionBorderSize();
+			AxisAlignedBB axisalignedbb = entity.getEntityBoundingBox().expand(borderSize, borderSize, borderSize);
+
+			// Player is inside an armor stand
+			if (axisalignedbb.isVecInside(eyes))
+				return (EntityArmorStand) entity;
+
+			MovingObjectPosition position = axisalignedbb.calculateIntercept(eyes, maxEyes);
+
+			// Not hitting
+			if (position == null)
+				continue;
+
+			double newDistance = eyes.distanceTo(position.hitVec);
+
+			// Make sure only the closest one is chosen
+			if (newDistance < distance) {
+				pointedEntity = entity;
+				distance = newDistance;
+			}
+		}
+
+		return (EntityArmorStand) pointedEntity;
+	}
 
 }

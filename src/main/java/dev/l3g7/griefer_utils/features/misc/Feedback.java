7c7e4c264132f393f4caef4b40e4653e69d24cac
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-10-07 23:39:03 +0200

    Changed files' encoding back to b64

--- src/main/java/dev/l3g7/griefer_utils/features/misc/Feedback.java
+++ src/main/java/dev/l3g7/griefer_utils/features/misc/Feedback.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URLConnection;
 import java.nio.file.Files;
+import java.util.Base64;
 import java.util.List;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -118,7 +119,7 @@ public class Feedback extends Feature {
 				JsonObject fileEntry = new JsonObject();
 				fileEntry.addProperty("name", file.getName());
 				fileEntry.addProperty("mime_type", URLConnection.guessContentTypeFromName(file.getName()));
-				fileEntry.addProperty("data", new String(Files.readAllBytes(file.toPath()), UTF_8));
+				fileEntry.addProperty("data", Base64.getEncoder().encodeToString(Files.readAllBytes(file.toPath())));
 
 				fileData.add(fileEntry);
 			}

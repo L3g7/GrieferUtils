514afd3d71067d69053caa4d5aa599e41e4db643
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-10-14 23:31:22 +0200

    Updated PlotChatIndicator to the new system (Now each CB has their own state)

--- src/main/java/dev/l3g7/griefer_utils/features/tweaks/PlotChatIndicator.java
+++ src/main/java/dev/l3g7/griefer_utils/features/tweaks/PlotChatIndicator.java
@@ -3,12 +3,9 @@ package dev.l3g7.griefer_utils.features.tweaks;
 import dev.l3g7.griefer_utils.event.event_bus.EventListener;
 import dev.l3g7.griefer_utils.event.events.chat.MessageReceiveEvent;
 import dev.l3g7.griefer_utils.event.events.server.CityBuildJoinEvent;
-import dev.l3g7.griefer_utils.event.events.server.ServerJoinEvent;
 import dev.l3g7.griefer_utils.event.events.server.ServerSwitchEvent;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
-import dev.l3g7.griefer_utils.misc.Config;
-import dev.l3g7.griefer_utils.misc.ServerCheck;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
 import dev.l3g7.griefer_utils.util.Reflection;
 import net.labymod.ingamechat.GuiChatCustom;
@@ -20,14 +17,16 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.lang.reflect.Array;
+import java.util.HashMap;
 
 @Singleton
 public class PlotChatIndicator extends Feature {
 
+	private final HashMap<String, Boolean> states = new HashMap<>();
+	private String server = "";
+
     private Boolean plotchatState = null;
     private boolean waitingForPlotchatStatus = false;
-
-    private String configPath = "tweaks.plot_chat_indicator.states." + uuid() + (".normal");
 
     private final BooleanSetting enabled = new BooleanSetting()
             .name("Plot-Chat-Indikator")
@@ -61,15 +60,11 @@ public class PlotChatIndicator extends Feature {
         if (!isActive() || plotchatState != null)
             return;
 
-        boolean onEventServer = false;
         ScorePlayerTeam team = world().getScoreboard().getTeam("server_value");
-        if (team != null)
-            onEventServer = team.getColorPrefix().equals("§fEvent");
+		server = team == null ? "" : team.getColorPrefix();
 
-        configPath = "tweaks.plot_chat_indicator.states." + uuid() + (onEventServer ? ".event" : ".normal");
-
-		if (Config.has(configPath)) {
-			plotchatState = Config.get(configPath).getAsBoolean();
+		if (states.containsKey(server)) {
+			plotchatState = states.get(server);
 			return;
 		}
 
@@ -85,9 +80,7 @@ public class PlotChatIndicator extends Feature {
         // Update plot chat state
         if (event.getFormatted().matches("^§r§8\\[§r§6GrieferGames§r§8] §r§.Die Einstellung §r§.chat §r§.wurde (?:de)?aktiviert\\.§r$")) {
             plotchatState = event.getFormatted().contains(" aktiviert");
-            // Save state along with player uuid so no problems occur when using multiple accounts
-            Config.set(configPath, plotchatState);
-            Config.save();
+			states.put(server, plotchatState);
 
             if (waitingForPlotchatStatus) {
                 waitingForPlotchatStatus = false;
@@ -102,27 +95,23 @@ public class PlotChatIndicator extends Feature {
             return;
 
         // Check if chat is open
-        if (event.type == RenderGameOverlayEvent.ElementType.CHAT) {
-            GuiScreen gcc = mc().currentScreen;
-            if (!(gcc instanceof GuiChat))
-                return;
-            int buttonWidth = gcc instanceof GuiChatCustom ? Array.getLength(Reflection.get(gcc, "chatButtons")) * 14 : 0;
+	    if (event.type != RenderGameOverlayEvent.ElementType.CHAT)
+			return;
 
-            int color = 0xFFFFA126; // TODO: this is ugly
+	    GuiScreen gcc = mc().currentScreen;
+	    if (!(gcc instanceof GuiChat))
+	        return;
 
-            // Render frame
-            GuiScreen.drawRect(2, gcc.height - 14, gcc.width - 2 - buttonWidth, gcc.height - 2, 100 << 24);
+	    int buttonWidth = gcc instanceof GuiChatCustom ? Array.getLength(Reflection.get(gcc, "chatButtons")) * 14 : 0;
+	    int color = 0xFFFFA126;
 
-            GuiScreen.drawRect(1, gcc.height - 15, gcc.width - 1 - buttonWidth, gcc.height - 14, color);
-            GuiScreen.drawRect(1, gcc.height - 2, gcc.width - 1 - buttonWidth, gcc.height - 1, color);
-            GuiScreen.drawRect(1, gcc.height - 15, 2, gcc.height - 1, color);
-            GuiScreen.drawRect(gcc.width - 2 - buttonWidth, gcc.height - 15, gcc.width - 1 - buttonWidth, gcc.height - 1, color);
-        }
+	    // Render frame
+	    GuiScreen.drawRect(2, gcc.height - 14, gcc.width - 2 - buttonWidth, gcc.height - 2, 100 << 24);
+
+	    GuiScreen.drawRect(1, gcc.height - 15, gcc.width - 1 - buttonWidth, gcc.height - 14, color);
+	    GuiScreen.drawRect(1, gcc.height - 2, gcc.width - 1 - buttonWidth, gcc.height - 1, color);
+	    GuiScreen.drawRect(1, gcc.height - 15, 2, gcc.height - 1, color);
+	    GuiScreen.drawRect(gcc.width - 2 - buttonWidth, gcc.height - 15, gcc.width - 1 - buttonWidth, gcc.height - 1, color);
     }
 
-    @EventListener
-    public void loadState(ServerJoinEvent ignored) {
-        if (ServerCheck.isOnGrieferGames() && Config.has(configPath))
-            plotchatState = Config.get(configPath).getAsBoolean();
-    }
 }

bee63730d5255df062b500b16f19f72fe5a57c2c
L3g7 <contact@l3g7.dev>
2022-11-27 17:50:32 +0100

    Fix TickScheduler synchronization

--- src/main/java/dev/l3g7/griefer_utils/util/misc/TickScheduler.java
+++ src/main/java/dev/l3g7/griefer_utils/util/misc/TickScheduler.java
@@ -24,7 +24,9 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -39,37 +41,47 @@ public class TickScheduler {
 	 * Runs the given runnable after the given delay in client ticks.
 	 */
 	public static void runAfterClientTicks(TRunnable runnable, int delay) {
-		clientTickTasks.put(runnable, new AtomicInteger(delay));
+		synchronized (clientTickTasks) {
+			clientTickTasks.put(runnable, new AtomicInteger(delay));
+		}
 	}
 
 	/**
 	 * Runs the given runnable after the given delay in render ticks.
 	 */
 	public static void runAfterRenderTicks(TRunnable runnable, int delay) {
-		renderTickTasks.put(runnable, new AtomicInteger(delay));
+		synchronized (renderTickTasks) {
+			renderTickTasks.put(runnable, new AtomicInteger(delay));
+		}
 	}
 
 	@EventListener
 	private static void onClientTick(ClientTickEvent event) {
-		updateTasks(clientTickTasks);
+		synchronized (clientTickTasks) {
+			updateTasks(clientTickTasks);
+		}
 	}
 
 	@EventListener
 	private static void onRenderTick(RenderTickEvent event) {
-		updateTasks(renderTickTasks);
+		synchronized (renderTickTasks) {
+			updateTasks(renderTickTasks);
+		}
 	}
 
 	/**
 	 * Decreases the ticks after which the task should run and runs it if ticks = 0.
 	 */
 	private static void updateTasks(Map<TRunnable, AtomicInteger> tasks) {
-		tasks.forEach((runnable, delay) -> {
+		Iterator<Entry<TRunnable, AtomicInteger>> it = tasks.entrySet().iterator();
+		while (it.hasNext()) {
 			// Decrease time, run if 0
-			if (tasks.get(runnable).decrementAndGet() == 0) {
-				tasks.remove(runnable);
-				runnable.run();
+			Entry<TRunnable, AtomicInteger> entry = it.next();
+			if (entry.getValue().decrementAndGet() == 0) {
+				it.remove();
+				entry.getKey().run();
 			}
-		});
+		}
 	}
 
 }

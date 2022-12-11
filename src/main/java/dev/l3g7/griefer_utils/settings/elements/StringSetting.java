2580a220fa51322312fa1f160ed7704d9b0dbb1e
L3g7 <contact@l3g7.dev>
2022-12-11 20:01:58 +0100

    Create StringSetting

---
+++ src/main/java/dev/l3g7/griefer_utils/settings/elements/StringSetting.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ *
+ * Copyright 2020-2022 L3g7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.l3g7.griefer_utils.settings.elements;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import dev.l3g7.griefer_utils.settings.ElementBuilder;
+import dev.l3g7.griefer_utils.settings.ValueHolder;
+import net.labymod.settings.elements.StringElement;
+
+/**
+ * A setting holding a string, represented in-game by a text input.
+ */
+public class StringSetting extends StringElement implements ElementBuilder<StringSetting>, ValueHolder<StringSetting, String> {
+
+	private final Storage<String> storage = new Storage<>(JsonPrimitive::new, JsonElement::getAsString, "");
+	private final IconStorage iconStorage = new IconStorage();
+
+	public StringSetting() {
+		super("§cNo name set", null, "", v -> {});
+		setSettingEnabled(true);
+		addCallback(this::set);
+	}
+
+	@Override
+	public Storage<String> getStorage() {
+		return storage;
+	}
+
+	@Override
+	public IconStorage getIconStorage() {
+		return iconStorage;
+	}
+
+	@Override
+	public void draw(int x, int y, int maxX, int maxY, int mouseX, int mouseY) {
+		super.draw(x, y, maxX, maxY, mouseX, mouseY);
+		drawIcon(x, y);
+	}
+
+}

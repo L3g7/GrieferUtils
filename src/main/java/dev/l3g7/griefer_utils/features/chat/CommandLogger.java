cb71046b09ac78a23b80916872be94dce6f489e3
L3g7 <contact@l3g7.dev>
2022-12-16 15:39:48 +0100

    Implement CommandLogger

---
+++ src/main/java/dev/l3g7/griefer_utils/features/chat/CommandLogger.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ *
+ * Copyright 2020-2022 L3g7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.l3g7.griefer_utils.features.chat;
+
+import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.event.events.MessageEvent.MessageSendEvent;
+import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.file_provider.Singleton;
+import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
+import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import net.labymod.utils.Material;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Singleton
+public class CommandLogger extends Feature {
+
+	@MainElement
+	private final BooleanSetting enabled = new BooleanSetting()
+		.name("Befehle loggen")
+		.description("Schreibt die gesendeten Befehle in den Log.")
+		.icon(Material.COMMAND);
+
+	private final Logger logger = LogManager.getLogger("CommandLogger");
+
+	@EventListener
+	private void onMessageSend(MessageSendEvent event) {
+		if (event.message.startsWith("/"))
+			logger.info("executed " + event.message);
+	}
+
+}

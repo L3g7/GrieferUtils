c6f1e87af69c08462db5885013b6320c66eb7285
L3g7 <contact@l3g7.dev>
2022-12-18 20:04:26 +0100

    Implement BorderSaver

---
+++ src/main/java/dev/l3g7/griefer_utils/features/item/BorderSaver.java
@@ -0,0 +1,113 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ *
+ * Copyright 2020-2022 L3g7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.l3g7.griefer_utils.features.item;
+
+import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.file_provider.Singleton;
+import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
+import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import dev.l3g7.griefer_utils.util.ItemUtil;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+
+import static dev.l3g7.griefer_utils.util.ItemUtil.createItem;
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.player;
+import static net.minecraftforge.event.entity.player.PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK;
+
+/**
+ * Suppresses left clicks and dropping when holing a diamond sword enchanted with looting 21.
+ */
+@Singleton
+public class BorderSaver extends Feature {
+
+	private static final int ACCEPT_SLOT_ID = 12, DECLINE_SLOT_ID = 14;
+
+	@MainElement
+	private final BooleanSetting enabled = new BooleanSetting()
+		.name("RandSaver")
+		.description("Fragt beim Einlösen eines Randes nach einer Bestätigung.")
+		.icon(createItem(Blocks.obsidian, 0, true));
+
+	private final IInventory inv = new InventoryBasic("§0Willst du den Rand einlösen?", false, 27);
+
+	public BorderSaver() {
+		ItemStack grayGlassPane = createItem(Blocks.stained_glass_pane, 7, "§8");
+
+		// Fill inventory with gray glass panes
+		for (int slot = 0; slot < 27; slot++)
+			inv.setInventorySlotContents(slot, grayGlassPane);
+
+		inv.setInventorySlotContents(ACCEPT_SLOT_ID, createItem(Items.dye, 10, "§aEinlösen"));
+		inv.setInventorySlotContents(DECLINE_SLOT_ID, createItem(Items.dye, 1, "§cAbbrechen"));
+	}
+
+	private boolean clickedOnBlock = false;
+
+	@EventListener
+	public void onPlayerInteract(PlayerInteractEvent event) {
+
+		// For some reason, a RIGHT_CLICK_AIR PlayerInteractEvent is triggered right after the RIGHT_CLICK_BLOCK event
+		if (clickedOnBlock) {
+			clickedOnBlock = false;
+			event.setCanceled(true);
+			return;
+		}
+
+		if (event.action != RIGHT_CLICK_BLOCK)
+			return;
+
+		ItemStack heldItem = player().getHeldItem();
+
+		if (heldItem == null || !heldItem.hasTagCompound())
+			return;
+
+		// Check if it's a border
+		if (!heldItem.getTagCompound().getBoolean("wall_effect"))
+			return;
+
+		event.setCanceled(true);
+		mc().displayGuiScreen(new GuiChest(player().inventory, inv) {
+
+			protected void handleMouseClick(Slot slot, int slotId, int btn, int type) {
+				if (slot != null)
+					slotId = slot.slotNumber;
+
+				if (slotId == DECLINE_SLOT_ID)
+					mc.thePlayer.closeScreenAndDropStack();
+
+				if (slotId != ACCEPT_SLOT_ID)
+					return;
+
+				mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.getHeldItem());
+				mc.displayGuiScreen(null);
+			}
+
+		});
+	}
+
+}

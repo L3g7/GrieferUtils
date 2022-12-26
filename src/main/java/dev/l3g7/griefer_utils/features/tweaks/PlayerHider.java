7f993643a8a73a995d6a139487119f65c19c5e80
L3g73 <103503837+L3g73@users.noreply.github.com>
2022-12-26 14:35:39 +0100

    Added option to exclude players from PlayerHider

--- src/main/java/dev/l3g7/griefer_utils/features/tweaks/PlayerHider.java
+++ src/main/java/dev/l3g7/griefer_utils/features/tweaks/PlayerHider.java
@@ -3,8 +3,11 @@ package dev.l3g7.griefer_utils.features.tweaks;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import dev.l3g7.griefer_utils.settings.elements.HeaderSetting;
+import dev.l3g7.griefer_utils.settings.elements.playerlist.PlayerListSetting;
 import dev.l3g7.griefer_utils.util.Reflection;
 import net.labymod.settings.elements.SettingsElement;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraftforge.client.event.RenderPlayerEvent;
@@ -13,6 +16,7 @@ import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +26,10 @@ public class PlayerHider extends Feature {
 	private static final List<String> BLOCKED_SOUNDS = Arrays.asList("random.eat", "random.burp", "random.drink");
 
 	private boolean playingOwnSounds = false;
+
+	private final PlayerListSetting excludedPlayers = new PlayerListSetting()
+		.name("%s. Spieler")
+		.config("tweaks.player_hider.excluded_players");
 
 	private final BooleanSetting enabled = new BooleanSetting()
 		.name("Spieler verstecken")
@@ -33,7 +41,10 @@ public class PlayerHider extends Feature {
 			if (isOnGrieferGames())
 				for (EntityPlayer player : world().playerEntities)
 					updatePlayer(player);
-		});
+		})
+		.subSettingsWithHeader("Spieler verstecken", new HeaderSetting().name("Ausgenommene Spieler"), excludedPlayers);
+
+	private final List<String> shownPlayers = new ArrayList<>();
 
 	public PlayerHider() {
 		super(Category.TWEAK);
@@ -58,7 +69,7 @@ public class PlayerHider extends Feature {
 	 */
 	@SubscribeEvent
 	public void onEntityRender(RenderPlayerEvent.Pre event) {
-		if (!isActive() || !isOnGrieferGames() || event.entity == player())
+		if (!isActive() || !isOnGrieferGames() || showPlayer(event.entity))
 			return;
 
 		event.setCanceled(true);
@@ -69,7 +80,7 @@ public class PlayerHider extends Feature {
 	 */
 	@SubscribeEvent
 	public void onSoundPlayAtEntity(PlaySoundAtEntityEvent event) {
-		if (event.entity.equals(player()))
+		if (showPlayer(event.entity))
 			playingOwnSounds = true;
 	}
 
@@ -91,29 +102,43 @@ public class PlayerHider extends Feature {
 	}
 
 	private void updatePlayer(EntityPlayer player) {
-		if (player == player())
+		if (player.equals(player()) || shownPlayers.contains(player.getName()))
 			return;
 
+		boolean hide = isActive();
+
+		// Ensure player is shown when its name was added while PlayerHider is active
+		String name = player.getName();
+		if (excludedPlayers.getNames().contains(name)) {
+			shownPlayers.add(name);
+			hide = false;
+		}
+
 		// Shadows
-		if (player.isInvisible() != isActive())
-			player.setInvisible(isActive() || player.isPotionActive(Potion.invisibility));
+		if (player.isInvisible() != hide)
+			player.setInvisible(hide || player.isPotionActive(Potion.invisibility));
 
 		// Fire
-		if (player.isImmuneToFire() != isActive())
-			Reflection.set(player, isActive(), "isImmuneToFire", "field_70178_ae", "ab");
+		if (player.isImmuneToFire() != hide)
+			Reflection.set(player, hide, "isImmuneToFire", "field_70178_ae", "ab");
 
 		// Sprinting particles
-		player.setSprinting(!isActive());
+		if (hide)
+			player.setSprinting(false);
 
 		// Effect particles
-		if (isActive())
+		if (hide)
 			Reflection.invoke(player, new String[] {"resetPotionEffectMetadata", "func_175133_bi", "bj"});
 		else
 			Reflection.invoke(player, new String[] {"updatePotionMetadata", "func_175135_B", "B"});
 
-		player.setSilent(isActive());
+		player.setSilent(hide);
 		player.setEating(false);
 		player.clearItemInUse();
+	}
+
+	private boolean showPlayer(Entity player) {
+		return player.equals(player()) || excludedPlayers.getNames().contains(player.getName());
 	}
 
 }

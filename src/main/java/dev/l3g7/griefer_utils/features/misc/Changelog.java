debee395a363e10b7fb526e4e26ef702783948fe
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-01-01 23:27:43 +0100

    Update Changelog sorting

--- src/main/java/dev/l3g7/griefer_utils/features/misc/Changelog.java
+++ src/main/java/dev/l3g7/griefer_utils/features/misc/Changelog.java
@@ -6,11 +6,16 @@ import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.features.misc.update_screen.UpdateScreen;
 import dev.l3g7.griefer_utils.file_provider.Singleton;
 import dev.l3g7.griefer_utils.misc.Constants;
+import dev.l3g7.griefer_utils.misc.VersionComparator;
 import dev.l3g7.griefer_utils.settings.elements.CategorySetting;
 import dev.l3g7.griefer_utils.settings.elements.HeaderSetting;
 import dev.l3g7.griefer_utils.settings.elements.TextSetting;
 import dev.l3g7.griefer_utils.util.IOUtil;
 import net.labymod.settings.elements.SettingsElement;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Singleton
 public class Changelog extends Feature {
@@ -31,6 +36,7 @@ public class Changelog extends Feature {
 		super(Category.MISC);
 
 		IOUtil.request("https://api.github.com/repos/L3g7/GrieferUtils/releases").asJsonArray(releases -> {
+			List<SettingsElement> entries = new ArrayList<>();
 			for (JsonElement releaseElement : releases) {
 				JsonObject release = releaseElement.getAsJsonObject();
 
@@ -43,15 +49,18 @@ public class Changelog extends Feature {
 
 				String title = "§l" + release.get("tag_name").getAsString();
 
-				element.subSettings(new CategorySetting()
-						.name(" " + title)
-						.subSettings(
-								new HeaderSetting("§r"),
-								new HeaderSetting("§r§e§l" + Constants.ADDON_NAME).scale(1.3),
-								new HeaderSetting("§f§lChangelog - " + title).scale(.7),
-								new TextSetting(release.get("body").getAsString().replace("\r", ""))
-						));
+				entries.add(new CategorySetting()
+					.name(" " + title)
+					.subSettings(
+						new HeaderSetting("§r"),
+						new HeaderSetting("§r§e§l" + Constants.ADDON_NAME).scale(1.3),
+						new HeaderSetting("§f§lChangelog - " + title).scale(.7),
+						new TextSetting(release.get("body").getAsString().replace("\r", ""))
+					));
 			}
+
+			entries.sort(Comparator.comparing(SettingsElement::getDisplayName, new VersionComparator()));
+			element.subSettings(entries);
 
 			element .name("§y§fChangelog")
 					.description(null)

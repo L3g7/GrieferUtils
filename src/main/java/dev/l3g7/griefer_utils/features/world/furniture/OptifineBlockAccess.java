9fd60c32febbef48285b833c8e46b34314431f12
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-01-01 20:36:39 +0100

    Update copyright

--- src/main/java/dev/l3g7/griefer_utils/features/world/furniture/OptifineBlockAccess.java
+++ src/main/java/dev/l3g7/griefer_utils/features/world/furniture/OptifineBlockAccess.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
  *
- * Copyright 2020-2022 L3g7
+ * Copyright 2020-2023 L3g7
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@
 /*
  * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
  *
- * Copyright 2020-2022 L3g7
+ * Copyright 2020-2023 L3g7
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

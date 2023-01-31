bbaea05df81bff6f5f4bd934f7f6afb9887864be
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-01-31 23:59:25 +0100

    Added HeadSaver

---
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/item/HeadSaver.java
@@ -0,0 +1,149 @@
+package dev.l3g7.griefer_utils.features.item;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.event.events.MessageEvent.MessageSendEvent;
+import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.features.world.ChestSearch;
+import dev.l3g7.griefer_utils.file_provider.Singleton;
+import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
+import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import dev.l3g7.griefer_utils.util.IOUtil;
+import dev.l3g7.griefer_utils.util.ItemUtil;
+import dev.l3g7.griefer_utils.util.misc.TickScheduler;
+import dev.l3g7.griefer_utils.util.misc.functions.Consumer;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.*;
+
+import static dev.l3g7.griefer_utils.util.ItemUtil.createItem;
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.*;
+
+@Singleton
+public class HeadSaver extends Feature {
+
+	private static final int ACCEPT_SLOT_ID = 11, PREVIEW_SLOT_ID = 13, DECLINE_SLOT_ID = 15;
+
+	private boolean accepted = false;
+
+	@MainElement
+	private final BooleanSetting enabled = new BooleanSetting()
+		.name("KopfSaver")
+		.description("Fragt beim Einlösen von /kopf nach einer Bestätigung.")
+		.icon(createItem(Items.skull, 3, false));
+
+	private final IInventory inv = new InventoryBasic(ChestSearch.marker + "§0Willst du /kopf einlösen?", false, 27);
+
+	public HeadSaver() {
+		ItemStack grayGlassPane = createItem(Blocks.stained_glass_pane, 7, "§8");
+
+		// Fill inventory with gray glass panes
+		for (int slot = 0; slot < 27; slot++)
+			inv.setInventorySlotContents(slot, grayGlassPane);
+
+		inv.setInventorySlotContents(ACCEPT_SLOT_ID, createItem(Items.dye, 10, "§aEinlösen"));
+		inv.setInventorySlotContents(PREVIEW_SLOT_ID, createItem(Items.skull, 0, "§3Vorschau anzeigen"));
+		inv.setInventorySlotContents(DECLINE_SLOT_ID, createItem(Items.dye, 1, "§cAbbrechen"));
+//		ItemSkull is = new ItemSkull();
+//		is.up
+	}
+
+	@EventListener
+	public void onMessageSend(MessageSendEvent event) {
+		if (!event.message.startsWith("/kopf "))
+			return;
+
+		if (accepted) {
+			accepted = false;
+			return;
+		}
+
+		String name = event.message.substring("/kopf ".length());
+		TickScheduler.runAfterRenderTicks(() -> displayScreen(name), 1);
+		event.setCanceled(true);
+	}
+
+	private void displayScreen(String name) {
+		GuiChest chest = new GuiChest(player().inventory, inv) {
+
+			protected void handleMouseClick(Slot slot, int slotId, int btn, int type) {
+				if (slot != null)
+					slotId = slot.slotNumber;
+
+				if (slotId == DECLINE_SLOT_ID)
+					mc.thePlayer.closeScreenAndDropStack();
+
+				if (slotId != ACCEPT_SLOT_ID)
+					return;
+
+				accepted = true;
+				send("/kopf " + name);
+				mc.thePlayer.closeScreenAndDropStack();
+			}
+
+		};
+
+		startTextureUpdate(chest, name);
+		mc().displayGuiScreen(chest);
+	}
+
+	private static void startTextureUpdate(GuiChest chest, String name) {
+		String nbtFormat = "{id:\"minecraft:skull\",Count:1b,tag:{%s},Damage:3s}";
+		String loreFormat = String.format(nbtFormat, "display:{Name:\"§fKopf von " + name + "\",Lore:[%s]}");
+
+		ItemStack skull = ItemUtil.createItem(Items.skull, 3, "§fKopf von " + name);
+
+		NBTTagCompound display = skull.getTagCompound().getCompoundTag("display");
+		NBTTagList lore = new NBTTagList();
+		lore.appendTag(new NBTTagString("§eTextur wird geladen..."));
+		display.setTag("Lore", lore);
+
+		chest.inventorySlots.inventorySlots.get(PREVIEW_SLOT_ID).putStack(skull);
+
+		requestTexture(name, tex -> {
+			if (tex == null) {
+				skull.readFromNBT(JsonToNBT.getTagFromJson(String.format(loreFormat, "§cTextur konnte nicht geladen werden!")));
+				chest.inventorySlots.inventorySlots.get(PREVIEW_SLOT_ID).putStack(skull);
+				return;
+			}
+
+			String textureTag = String.format(nbtFormat, "display:{Name:\"§fKopf von " + name + "\"},SkullOwner:{Properties:{textures:[0:{Value:\"%s\"}]},Name:\"" + name + "\"}");
+			skull.readFromNBT(JsonToNBT.getTagFromJson(String.format(textureTag, tex)));
+			chest.inventorySlots.inventorySlots.get(PREVIEW_SLOT_ID).putStack(skull);
+		});
+	}
+
+	private static void requestTexture(String name, Consumer<String> textureConsumer) {
+		String url = "https://api.mojang.com/users/profiles/minecraft/" + name;
+		IOUtil.read(url).asJsonObject(playerData -> {
+			if (!playerData.has("id")) {
+				textureConsumer.accept(null);
+				return;
+			}
+
+			String uuid = playerData.get("id").getAsString();
+			IOUtil.read("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid).asJsonObject(textureData -> {
+				if (!textureData.has("properties")) {
+					textureConsumer.accept(null);
+					return;
+				}
+
+				for (JsonElement element : textureData.getAsJsonArray("properties")) {
+					JsonObject property = element.getAsJsonObject();
+
+					if (property.has("name") && property.get("name").getAsString().equals("textures")) {
+						textureConsumer.accept(property.get("value").getAsString());
+						return;
+					}
+				}
+			});
+		}).orElse(() -> textureConsumer.accept(null));
+	}
+
+}

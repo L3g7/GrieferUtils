ccc8a51516854716a217cce0132029a1c852773b
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-02-18 19:49:58 +0100

    Added support for the adventurer to SCIS

--- addon/src/main/java/dev/l3g7/griefer_utils/features/item/SuppressCompressedItemSell.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/item/SuppressCompressedItemSell.java
@@ -19,6 +19,7 @@
 package dev.l3g7.griefer_utils.features.item;
 
 import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.event.events.WindowClickEvent;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
@@ -46,6 +47,8 @@ import static dev.l3g7.griefer_utils.util.MinecraftUtil.player;
 @Singleton
 public class SuppressCompressedItemSell extends Feature {
 
+	private static final ItemStack blocked;
+
 	@MainElement
 	private final BooleanSetting enabled = new BooleanSetting()
 		.name("Abgeben komprimierter Items deaktivieren")
@@ -53,18 +56,45 @@ public class SuppressCompressedItemSell extends Feature {
 		.icon(ItemUtil.createItem(Blocks.gold_block, 0, true));
 
 	@EventListener
+	public void onWindowClick(WindowClickEvent event) {
+		if (!(mc().currentScreen instanceof GuiChest))
+			return;
+
+		if (event.itemStack == null || !event.itemStack.hasTagCompound())
+			return;
+
+		IInventory lowerChestInventory = Reflection.get(mc().currentScreen, "lowerChestInventory");
+		String title = lowerChestInventory.getDisplayName().getFormattedText();
+		if (!title.startsWith("§6Adventure-Jobs"))
+			return;
+
+
+		if (event.itemStack.getTagCompound().hasKey("compressionLevel"))
+			event.setCanceled(true);
+	}
+
+	@EventListener
 	public void onMouseGui(GuiScreenEvent.MouseInputEvent.Pre event) {
 		if (!(event.gui instanceof GuiChest))
 			return;
 
+		suppressOrbs(event);
+		suppressJobs(event);
+	}
+
+	private void suppressOrbs(GuiScreenEvent.MouseInputEvent.Pre event) {
 		IInventory lowerChestInventory = Reflection.get(event.gui, "lowerChestInventory");
 		String title = lowerChestInventory.getDisplayName().getFormattedText();
 		if (!title.startsWith("§6Orbs") && !title.equals("§cGeblockt!§r"))
 			return;
 
 		ItemStack sellingItem = lowerChestInventory.getStackInSlot(11);
+
 		boolean hasMatchingCompressedItem = false;
 		for (ItemStack itemStack : player().inventory.mainInventory) {
+			if (sellingItem == null) // Break here to so the canceling still happens
+				break;
+
 			if (itemStack == null || itemStack.getTagCompound() == null) continue;
 
 			if (sellingItem.isItemEqual(itemStack) && itemStack.getTagCompound().hasKey("compressionLevel")) {
@@ -73,10 +103,6 @@ public class SuppressCompressedItemSell extends Feature {
 			}
 		}
 		if (hasMatchingCompressedItem) {
-			ItemStack blocked = ItemUtil.createItem(Blocks.stained_glass_pane, 14, "§c§lGeblockt!");
-			NBTTagList l = blocked.getTagCompound().getCompoundTag("display").getTagList("Lore", Constants.NBT.TAG_STRING);;
-			l.appendTag(new NBTTagString("§cDu hast ein komprimiertes Item im Inventar!"));
-			blocked.getTagCompound().getCompoundTag("display").setTag("Lore", l);
 			((InventoryBasic) lowerChestInventory).setCustomName("§cGeblockt!");
 			for (int i = 0; i < lowerChestInventory.getSizeInventory(); i++) {
 				if (i != 45)
@@ -89,6 +115,46 @@ public class SuppressCompressedItemSell extends Feature {
 			Slot slot = ((GuiContainer) mc().currentScreen).getSlotUnderMouse();
 			event.setCanceled(slot != null && slot.getSlotIndex() != 45);
 		}
+	}
+
+	private void suppressJobs(GuiScreenEvent.MouseInputEvent.Pre event) {
+		IInventory lowerChestInventory = Reflection.get(event.gui, "lowerChestInventory");
+		String title = lowerChestInventory.getDisplayName().getFormattedText();
+		if (!title.startsWith("§6Adventure-Jobs"))
+			return;
+
+		for (int i : new int[] {10, 13, 16}) {
+			ItemStack sellingItem = lowerChestInventory.getStackInSlot(i);
+			if (sellingItem == null)
+				continue;
+
+			boolean hasMatchingCompressedItem = false;
+			for (ItemStack itemStack : player().inventory.mainInventory) {
+				if (itemStack == null || itemStack.getTagCompound() == null) continue;
+
+				if (sellingItem.isItemEqual(itemStack) && itemStack.getTagCompound().hasKey("compressionLevel")) {
+					hasMatchingCompressedItem = true;
+					break;
+				}
+			}
+
+			if (hasMatchingCompressedItem) {
+				lowerChestInventory.setInventorySlotContents(i, blocked);
+				event.setCanceled(true);
+			}
+
+		}
+
+		Slot slot = ((GuiContainer) mc().currentScreen).getSlotUnderMouse();
+		event.setCanceled(slot != null && slot.getHasStack() && slot.getStack() == blocked);
+
+	}
+
+	static {
+		blocked = ItemUtil.createItem(Blocks.stained_glass_pane, 14, "§c§lGeblockt!");
+		NBTTagList l = blocked.getTagCompound().getCompoundTag("display").getTagList("Lore", Constants.NBT.TAG_STRING);
+		l.appendTag(new NBTTagString("§cDu hast ein komprimiertes Item im Inventar!"));
+		blocked.getTagCompound().getCompoundTag("display").setTag("Lore", l);
 	}
 
 }

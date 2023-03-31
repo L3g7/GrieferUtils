636c22746587630eeecaad32fb9b0078732d67bf
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-03-31 19:55:42 +0200

    Added remove luckyblock- and remove caseopening-notifications to ChatMods

--- addon/src/main/java/dev/l3g7/griefer_utils/features/chat/ChatMods.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/chat/ChatMods.java
@@ -19,13 +19,15 @@
 package dev.l3g7.griefer_utils.features.chat;
 
 import com.google.common.collect.ImmutableList;
+import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.event.EventListener;
 import dev.l3g7.griefer_utils.features.Feature;
-import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
 import dev.l3g7.griefer_utils.settings.elements.DropDownSetting;
+import dev.l3g7.griefer_utils.util.ItemUtil;
 import net.labymod.utils.Material;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 
 import java.util.List;
@@ -60,16 +62,24 @@ public class ChatMods extends Feature {
 		.name("Streamer-Benachrichtigungen entfernen")
 		.icon("twitch");
 
-	private final BooleanSetting muteMysteryMod = new BooleanSetting()
+	private final BooleanSetting removeMysteryMod = new BooleanSetting()
 		.name("Download-Benachrichtigungen entfernen")
 		.icon("mysterymod")
 		.defaultValue(true);
+
+	private final BooleanSetting removeLuckyBlock = new BooleanSetting()
+		.name("LuckyBlock-Benachrichtigungen entfernen")
+		.icon(ItemUtil.createItem(Blocks.gold_block, 0, true));
+
+	private final BooleanSetting removeCaseOpening = new BooleanSetting()
+		.name("CaseOpening-Benachrichtigungen entfernen")
+		.icon("chest");
 
 	@MainElement
 	private final BooleanSetting enabled = new BooleanSetting()
 		.name("ChatMods")
 		.icon("speech_bubble")
-		.subSettings(antiClearChat, removeSupremeSpaces, removeStreamerNotifications, muteMysteryMod, news);
+		.subSettings(antiClearChat, removeSupremeSpaces, removeStreamerNotifications, removeMysteryMod, removeLuckyBlock, removeCaseOpening, news);
 
 
 	private boolean isNews = false;
@@ -100,7 +110,15 @@ public class ChatMods extends Feature {
 
 		// Remove MysteryMod download notification
 		if (!event.isCanceled())
-			event.setCanceled(muteMysteryMod.get() && MYSTERY_MOD_DOWNLOAD_NOTIFICATION.contains(event.message.getFormattedText()));
+			event.setCanceled(removeMysteryMod.get() && MYSTERY_MOD_DOWNLOAD_NOTIFICATION.contains(event.message.getFormattedText()));
+
+		// remove luckyblock
+		if (!event.isCanceled())
+			event.setCanceled(removeLuckyBlock.get() && event.message.getFormattedText().startsWith("§r§8[§r§e§lLu§r§6§lck§r§e§lyB§r§6§llo§r§e§lck§r§8]"));
+
+		// remove case opening
+		if (!event.isCanceled())
+			event.setCanceled(removeCaseOpening.get() && event.message.getFormattedText().startsWith("§r§8[§r§bCase§r§fOpening§r§8]"));
 	}
 
 	private enum NewsMode {

d5e471f8daf6e31983d2f38d14bab76436eb1865
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-04-05 20:06:20 +0200

    Added support for compressed items to InventoryBlockSelection

--- addon/src/main/java/dev/l3g7/griefer_utils/features/world/InventoryBlockSelection.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/world/InventoryBlockSelection.java
@@ -22,8 +22,11 @@ import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.event.EventListener;
 import dev.l3g7.griefer_utils.event.events.BlockPickEvent;
 import dev.l3g7.griefer_utils.features.Feature;
+import dev.l3g7.griefer_utils.features.item.item_saver.ItemSaver;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import dev.l3g7.griefer_utils.util.ItemUtil;
+import net.labymod.utils.Material;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 
@@ -33,19 +36,20 @@ import static dev.l3g7.griefer_utils.util.MinecraftUtil.player;
 @Singleton
 public class InventoryBlockSelection extends Feature {
 
+	private final BooleanSetting compressed = new BooleanSetting()
+		.name("Komprimierte Items")
+		.description("Ob komprimierte Items bei der Blockauswahl auch ausgewählt werden sollen.")
+		.icon(Material.STONE);
+
 	@MainElement
 	private final BooleanSetting enabled = new BooleanSetting()
 		.name("Blockauswahl mit Inventar")
 		.description("Erweitert die Blockauswahl um das Inventar.")
-		.icon("middle_click");
+		.icon("middle_click")
+		.subSettings(compressed);
 
 	@EventListener
 	public void onBlockPick(BlockPickEvent event) {
-		// Check if it's not already in the hotbar
-		for (int slot = 0; slot < 9; slot++)
-			if (isStackInSlot(event.requiredStack, slot))
-				return;
-
 		InventoryPlayer inv = player().inventory;
 
 		// If possible, the stack is put in an empty slot
@@ -53,24 +57,51 @@ public class InventoryBlockSelection extends Feature {
 		if (targetSlot == -1 || targetSlot >= 9)
 			targetSlot = inv.currentItem;
 
-		for (int slot = 9; slot < 36; slot++) {
-			if (isStackInSlot(event.requiredStack, slot)) {
-				// Move stack
-				mc().playerController.windowClick(0, slot, targetSlot, 2, player());
+		int bestSlot = 0;
+		int bestScore = -1;
 
-				// Switch selected slot
-				if (targetSlot != inv.currentItem)
-					inv.currentItem = targetSlot;
-
-				event.setCanceled(true);
-				return;
+		for (int slot = 0; slot < 36; slot++) {
+			int score = getScore(event.requiredStack, slot);
+			if (bestScore < score) {
+				bestScore = score;
+				bestSlot = slot;
 			}
 		}
+
+		if (bestScore == -1 || inv.currentItem == bestSlot)
+			return;
+
+		if (bestSlot < 9) { // Best slot is in hotbar
+			inv.currentItem = bestSlot;
+			event.setCanceled(true);
+			return;
+		}
+
+		// Move stack
+		mc().playerController.windowClick(0, bestSlot, targetSlot, 2, player());
+
+		if (inv.currentItem != targetSlot)
+			inv.currentItem = targetSlot;
+
+		event.setCanceled(true);
 	}
 
-	private static boolean isStackInSlot(ItemStack requiredStack, int slot) {
+	private int getScore(ItemStack requiredStack, int slot) {
 		ItemStack stack = player().inventory.getStackInSlot(slot);
-		return stack != null && stack.isItemEqual(requiredStack) && ItemStack.areItemStackTagsEqual(stack, requiredStack);
+		if (stack == null || !stack.isItemEqual(requiredStack) || ItemSaver.getSetting(stack) != null)
+			return -1;
+
+		if (!compressed.get())
+			return 0;
+
+		if (!stack.hasTagCompound() || !stack.getTagCompound().hasKey("stackSize"))
+			return Integer.MAX_VALUE;
+
+		if (!stack.getTagCompound().getBoolean("warningDisplayed"))
+			return 0;
+
+		String line = ItemUtil.getLore(stack).get(0);
+		return Integer.parseInt(line.substring(12).replace(".", ""));
 	}
 
 

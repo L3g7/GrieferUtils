f7e39bb762eee11ade23b456e062a9a6015eff99
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-04-15 16:17:44 +0200

    Added Bank- & CoinBalance

---
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/modules/balances/BankBalance.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ *
+ * Copyright 2020-2023 L3g7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.l3g7.griefer_utils.features.modules.balances;
+
+import dev.l3g7.griefer_utils.core.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.misc.Constants;
+import dev.l3g7.griefer_utils.features.Module;
+import dev.l3g7.griefer_utils.features.player.scoreboard.BankScoreboard;
+import net.labymod.settings.elements.ControlElement;
+
+@Singleton
+public class BankBalance extends Module {
+
+	public BankBalance() {
+		super("Bankguthaben", "Zeigt das Bankguthaben an.", "bank", new ControlElement.IconData("griefer_utils/icons/bank.png"));
+	}
+
+	@Override
+	public String[] getValues() {
+		if (BankScoreboard.getBankBalance() == -1)
+			return getDefaultValues();
+
+		return new String[] {Constants.DECIMAL_FORMAT_98.format(BankScoreboard.getBankBalance()) + "$"};
+	}
+
+	@Override
+	public String[] getDefaultValues() {
+		return new String[] {"?"};
+	}
+
+}

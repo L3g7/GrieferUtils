df82e405321a5369d311bdfcba2c959447de8d05
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-04-16 18:57:39 +0200

    Added Schematica support to RedstoneHelper (and fixed its performance)

--- addon/src/main/java/dev/l3g7/griefer_utils/features/world/RedstoneHelper.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/world/RedstoneHelper.java
@@ -18,14 +18,20 @@
 
 package dev.l3g7.griefer_utils.features.world;
 
+import com.github.lunatrius.schematica.api.ISchematic;
+import dev.l3g7.griefer_utils.core.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.misc.Vec3d;
+import dev.l3g7.griefer_utils.core.reflection.Reflection;
 import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.event.events.ChunkFilledEvent;
+import dev.l3g7.griefer_utils.event.events.MessageEvent;
+import dev.l3g7.griefer_utils.event.events.network.PacketEvent;
+import dev.l3g7.griefer_utils.event.events.network.ServerEvent;
 import dev.l3g7.griefer_utils.event.events.render.ParticleSpawnEvent;
 import dev.l3g7.griefer_utils.features.Feature;
-import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
 import dev.l3g7.griefer_utils.settings.elements.CategorySetting;
-import dev.l3g7.griefer_utils.core.misc.Vec3d;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDispenser;
 import net.minecraft.block.BlockRedstoneWire;
@@ -34,17 +40,24 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.S22PacketMultiBlockChange;
+import net.minecraft.network.play.server.S23PacketBlockChange;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.event.world.ChunkEvent;
 
-import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.*;
 import static net.labymod.utils.Material.COMPASS;
 import static net.labymod.utils.Material.REDSTONE;
+import static net.minecraft.init.Blocks.*;
 import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
 
@@ -54,23 +67,29 @@ import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
 @Singleton
 public class RedstoneHelper extends Feature {
 
-	private static final int REDSTONE_PARTICLE_ID = EnumParticleTypes.REDSTONE.getParticleID();
+	private static final Map<ChunkCoordIntPair, Map<BlockPos, RedstoneRenderObject>> redstoneRenderObjects = new HashMap<>();
+	private static final Map<BlockPos, RedstoneRenderObject> schematicasRROs = new HashMap<>();
+	private static final Class<?> clientProxyClass;
 
-	private final BooleanSetting showZeroPower = new BooleanSetting()
+	private static final int REDSTONE_PARTICLE_ID = EnumParticleTypes.REDSTONE.getParticleID();
+	private static Object previousSchematic = null;
+	private static BlockPos previousSchematicPos = null;
+
+	private static final BooleanSetting showZeroPower = new BooleanSetting()
 		.name("0 anzeigen")
 		.icon(REDSTONE);
 
-	private final BooleanSetting showPower = new BooleanSetting()
+	private static final BooleanSetting showPower = new BooleanSetting()
 		.name("Redstone-Stärke anzeigen")
 		.icon(REDSTONE)
 		.subSettings(showZeroPower);
 
-	private final BooleanSetting showDirection = new BooleanSetting()
+	private static final BooleanSetting showDirection = new BooleanSetting()
 		.name("Richtung anzeigen")
 		.description("Zeigt die Richtung von Werfern / Spendern und Trichtern.")
 		.icon(COMPASS);
 
-	private final BooleanSetting hideRedstoneParticles = new BooleanSetting()
+	private static final BooleanSetting hideRedstoneParticles = new BooleanSetting()
 		.name("Redstone-Partikel verstecken")
 		.icon(REDSTONE)
 		.defaultValue(true);
@@ -80,6 +99,108 @@ public class RedstoneHelper extends Feature {
 		.name("Redstone")
 		.icon(REDSTONE)
 		.subSettings(showPower, showDirection, hideRedstoneParticles);
+
+	@EventListener
+	public void onChunkFilled(ChunkFilledEvent event) {
+		Map<BlockPos, RedstoneRenderObject> renderObjects = new HashMap<>();
+		ChunkCoordIntPair coords = new ChunkCoordIntPair(event.getChunk().xPosition, event.getChunk().zPosition);
+
+		for (ExtendedBlockStorage ebs : event.getChunk().getBlockStorageArray()) {
+			if (ebs == null)
+				continue;
+
+			for (int x = 0; x < 16; x++) {
+				for (int y = 0; y < 16; y++) {
+					for (int z = 0; z < 16; z++) {
+						RedstoneRenderObject redstoneRenderObject = RedstoneRenderObject.fromState(ebs.get(x, y, z));
+						if (redstoneRenderObject == null)
+							continue;
+
+						renderObjects.put(coords.getBlock(x, y + ebs.getYLocation(), z), redstoneRenderObject);
+					}
+				}
+			}
+		}
+
+		if (!renderObjects.isEmpty())
+			redstoneRenderObjects.put(coords, renderObjects);
+	}
+
+	@EventListener
+	public void onChunkUnload(ChunkEvent.Unload event) {
+		ChunkCoordIntPair coordPair = new ChunkCoordIntPair(event.getChunk().xPosition, event.getChunk().zPosition);
+		redstoneRenderObjects.remove(coordPair);
+	}
+
+	@EventListener
+	public void onServerChange(ServerEvent.ServerSwitchEvent event) {
+		redstoneRenderObjects.clear();
+	}
+
+	@EventListener
+	public void onPacket(PacketEvent.PacketReceiveEvent event) {
+		if (event.packet instanceof S23PacketBlockChange) {
+			S23PacketBlockChange packet = (S23PacketBlockChange) event.packet;
+			onBlockUpdate(packet.getBlockPosition(), packet.getBlockState());
+		}
+
+		if (!(event.packet instanceof S22PacketMultiBlockChange))
+			return;
+
+		S22PacketMultiBlockChange packet = (S22PacketMultiBlockChange) event.packet;
+		for (S22PacketMultiBlockChange.BlockUpdateData data : packet.getChangedBlocks())
+			onBlockUpdate(data.getPos(), data.getBlockState());
+	}
+
+	private void onBlockUpdate(BlockPos pos, IBlockState state) {
+		RedstoneRenderObject redstoneRenderObject = RedstoneRenderObject.fromState(state);
+
+		ChunkCoordIntPair pair = new ChunkCoordIntPair(pos.getX() >> 4, pos.getZ() >> 4);
+
+		if (redstoneRenderObject != null) {
+			Map<BlockPos, RedstoneRenderObject> map = redstoneRenderObjects.computeIfAbsent(pair, k -> new HashMap<>());
+			map.put(pos, RedstoneRenderObject.fromState(state));
+			return;
+		}
+
+		Map<BlockPos, RedstoneRenderObject> map = redstoneRenderObjects.get(pair);
+		if (map == null)
+			return;
+
+		map.remove(pos);
+		if (map.isEmpty())
+			redstoneRenderObjects.remove(pair);
+	}
+
+	private void updateSchematic() {
+		Object schematicWorld = Reflection.get(clientProxyClass, "schematic");
+
+		ISchematic schematic = schematicWorld == null ? null : Reflection.get(schematicWorld, "schematic");
+		BlockPos position = schematicWorld == null ? null : Reflection.get(schematicWorld, "position");
+
+		if (previousSchematic == schematic && Objects.equals(previousSchematicPos, position))
+			return;
+
+		schematicasRROs.clear();
+		previousSchematic = schematic;
+		previousSchematicPos = position.up().down();
+
+		if (schematic == null)
+			return;
+
+		for (int dX = 0; dX < schematic.getWidth(); dX++) {
+			for (int dY = 0; dY < schematic.getHeight(); dY++) {
+				for (int dZ = 0; dZ < schematic.getLength(); dZ++) {
+					IBlockState state = schematic.getBlockState(new BlockPos(dX, dY, dZ));
+					RedstoneRenderObject redstoneRenderObject = RedstoneRenderObject.fromState(state);
+					if (redstoneRenderObject == null)
+						continue;
+
+					schematicasRROs.put(position.add(dX, dY, dZ), redstoneRenderObject);
+				}
+			}
+		}
+	}
 
 	// hide Redstone dust
 	// Repeater delay
@@ -92,36 +213,22 @@ public class RedstoneHelper extends Feature {
 
 	@EventListener
 	public void onRenderWorldLast(RenderWorldLastEvent event) {
-		if (!showPower.get() && !showDirection.get())
+		if (!showPower.get() && !showDirection.get() || (redstoneRenderObjects.isEmpty() && schematicasRROs.isEmpty()))
 			return;
+
+		if (clientProxyClass != null)
+			updateSchematic();
 
 		GlStateManager.disableDepth();
 		GlStateManager.disableCull();
 		GlStateManager.disableTexture2D();
-		EntityPlayer p = Minecraft.getMinecraft().thePlayer;
-		int px = (int) p.posX;
-		int py = (int) p.posY;
-		int pz = (int) p.posZ;
 
-		int chunkXEnd = (px >> 4) + 2;
-		int chunkZEnd = (pz >> 4) + 2;
-		for (int cx = (px >> 4) - 2; cx < chunkXEnd; cx++) {
-			for (int cz = (pz >> 4) - 2; cz < chunkZEnd; cz++) {
-				Chunk chunk = Minecraft.getMinecraft().theWorld.getChunkProvider().provideChunk(cx, cz);
-				int chunkYEnd = Math.min((py >>> 4) + 2, 15);
-				for (int cy = (py >>> 4) - 2; cy < chunkYEnd; cy++) {
-					if (cy < 0) cy = 0;
-					ExtendedBlockStorage ebs = chunk.getBlockStorageArray()[cy];
-					if (ebs == null)
-						continue;
+		for (Map<BlockPos, RedstoneRenderObject> map : redstoneRenderObjects.values())
+			for (Map.Entry<BlockPos, RedstoneRenderObject> entry : map.entrySet())
+				entry.getValue().render(entry.getKey(), event.partialTicks);
 
-					for (int x = 0; x < 16; x++)
-						for (int y = 0; y < 16; y++)
-							for (int z = 0; z < 16; z++)
-								processBlock(cx << 4 | x, cy << 4 | y, cz << 4 | z, event.partialTicks, ebs.get(x, y, z));
-				}
-			}
-		}
+		for (Map.Entry<BlockPos, RedstoneRenderObject> entry : schematicasRROs.entrySet())
+			entry.getValue().render(entry.getKey(), event.partialTicks);
 
 		GlStateManager.disableLighting();
 		GlStateManager.enableTexture2D();
@@ -129,44 +236,94 @@ public class RedstoneHelper extends Feature {
 		GlStateManager.enableCull();
 	}
 
-	private void processBlock(int x, int y, int z, float partialTicks, IBlockState state) {
-		Block block = state.getBlock();
+	@EventListener
+	public void onMsgs(MessageEvent.MessageSendEvent event) {
+		if (event.message.equals("//rh")) {
+			display(schematicasRROs.size() + "");
+			event.setCanceled(true);
+		}
+	}
 
-		// Render power
-		if (showPower.get()) {
-			if (block == Blocks.redstone_wire) {
-				int power = state.getValue(BlockRedstoneWire.POWER);
-				if (power > 0 || showZeroPower.get()) {
-					FontRenderer font = mc().fontRendererObj;
+	private abstract static class RedstoneRenderObject {
 
-					prepareRender(new Vec3d(x, y + 0.02, z), partialTicks);
-					String str = String.valueOf(power);
+		public static RedstoneRenderObject fromState(IBlockState state) {
+			Block block = state.getBlock();
+			if (block == redstone_wire)
+				return new Wire(state.getValue(BlockRedstoneWire.POWER));
 
-					GlStateManager.scale(0.035, 0.035, 0.035);
-					GlStateManager.rotate(90, 1, 0, 0);
-					GlStateManager.rotate(180 + mc().getRenderManager().playerViewY, 0, 0, 1);
-					GlStateManager.translate(-(font.getStringWidth(str) - 1) / 2d, -(font.FONT_HEIGHT / 2d - 1), 0);
+			boolean isHopper = block == hopper;
+			if (!isHopper && block != dropper && block != dispenser)
+				return null;
 
-					font.drawString(str, 0, 0, 0xFFFFFF);
+			EnumFacing dir = state.getValue(BlockDispenser.FACING);
+			if (isHopper && dir == EnumFacing.DOWN)
+				return null;
 
-					GlStateManager.popMatrix();
-				}
+			return new Directional(dir, isHopper);
+
+		}
+
+		private static void prepareRender(Vec3d loc, float partialTicks) {
+			GlStateManager.pushMatrix();
+			Entity viewer = Minecraft.getMinecraft().getRenderViewEntity();
+			double viewerX = viewer.lastTickPosX + (viewer.posX - viewer.lastTickPosX) * partialTicks;
+			double viewerY = viewer.lastTickPosY + (viewer.posY - viewer.lastTickPosY) * partialTicks;
+			double viewerZ = viewer.lastTickPosZ + (viewer.posZ - viewer.lastTickPosZ) * partialTicks;
+			double tx = (int) loc.x - viewerX + 0.5;
+			double ty = loc.y - viewerY;
+			double tz = (int) loc.z - viewerZ + 0.5;
+			GlStateManager.translate(tx, ty, tz);
+
+			GlStateManager.enableDepth();
+			GlStateManager.tryBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+			GlStateManager.enableTexture2D();
+		}
+
+		public abstract void render(BlockPos pos, float partialTicks);
+
+		private static class Wire extends RedstoneRenderObject {
+			private final int power;
+
+			private Wire(int power) {
+				this.power = power;
+			}
+
+			public void render(BlockPos pos, float partialTicks) {
+				if (!showPower.get())
+					return;
+
+				if (power <= 0 && !showZeroPower.get())
+					return;
+
+				FontRenderer font = mc().fontRendererObj;
+
+				prepareRender(new Vec3d(pos.getX(), pos.getY() + 0.02, pos.getZ()), partialTicks);
+				String str = String.valueOf(power);
+
+				GlStateManager.scale(0.035, 0.035, 0.035);
+				GlStateManager.rotate(90, 1, 0, 0);
+				GlStateManager.rotate(180 + mc().getRenderManager().playerViewY, 0, 0, 1);
+				GlStateManager.translate(-(font.getStringWidth(str) - 1) / 2d, -(font.FONT_HEIGHT / 2d - 1), 0);
+
+				font.drawString(str, 0, 0, 0xFFFFFF);
+
+				GlStateManager.popMatrix();
 			}
 		}
 
-		// Render direction
-		if (showDirection.get()) {
-			boolean isHopper = block == Blocks.hopper;
-			if (isHopper || block == Blocks.dropper || block == Blocks.dispenser) {
-				EnumFacing dirVec = state.getValue(BlockDispenser.FACING);
+		private static class Directional extends RedstoneRenderObject {
+			private final EnumFacing dir;
+			private final boolean isHopper;
 
-				if (isHopper && dirVec.getAxis() == EnumFacing.Axis.Y)
-					return;
+			private Directional(EnumFacing dir, boolean isHopper) {
+				this.dir = dir;
+				this.isHopper = isHopper;
+			}
 
-				Vec3d loc = new Vec3d(x, y, z);
+			public void render(BlockPos pos, float partialTicks) {
+				prepareRender(new Vec3d(pos.getX(), pos.getY(), pos.getZ()), partialTicks);
 
-				prepareRender(loc, partialTicks);
-				switch (dirVec) {
+				switch (dir) {
 					case UP:
 						GlStateManager.rotate(270, 0, 0, 1);
 						GlStateManager.translate(-0.51, -0.51, 0);
@@ -215,20 +372,14 @@ public class RedstoneHelper extends Feature {
 		}
 	}
 
-	private void prepareRender(Vec3d loc, float partialTicks) {
-		GlStateManager.pushMatrix();
-		Entity viewer = Minecraft.getMinecraft().getRenderViewEntity();
-		double viewerX = viewer.lastTickPosX + (viewer.posX - viewer.lastTickPosX) * partialTicks;
-		double viewerY = viewer.lastTickPosY + (viewer.posY - viewer.lastTickPosY) * partialTicks;
-		double viewerZ = viewer.lastTickPosZ + (viewer.posZ - viewer.lastTickPosZ) * partialTicks;
-		double tx = (int) loc.x - viewerX + 0.5;
-		double ty = loc.y - viewerY;
-		double tz = (int) loc.z - viewerZ + 0.5;
-		GlStateManager.translate(tx, ty, tz);
-
-		GlStateManager.enableDepth();
-		GlStateManager.tryBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-		GlStateManager.enableTexture2D();
+	static {
+		Class<?> tempClientProxyClass;
+		try {
+			tempClientProxyClass = Class.forName("com.github.lunatrius.schematica.proxy.ClientProxy");
+		} catch (ClassNotFoundException cnfe) {
+			tempClientProxyClass = null;
+		}
+		clientProxyClass = tempClientProxyClass;
 	}
 
 }

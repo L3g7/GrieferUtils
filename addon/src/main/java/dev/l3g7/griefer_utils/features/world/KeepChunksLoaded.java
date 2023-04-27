51a3376d5ee7020eff572297a59d9212d553dd7b
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-04-27 21:42:38 +0200

    Added a warning when trying to enable KeepChunksLoaded with OptiFine

--- addon/src/main/java/dev/l3g7/griefer_utils/features/world/KeepChunksLoaded.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/world/KeepChunksLoaded.java
@@ -20,6 +20,7 @@ package dev.l3g7.griefer_utils.features.world;
 
 import dev.l3g7.griefer_utils.core.file_provider.FileProvider;
 import dev.l3g7.griefer_utils.core.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.misc.Constants;
 import dev.l3g7.griefer_utils.core.reflection.Reflection;
 import dev.l3g7.griefer_utils.core.util.Util;
 import dev.l3g7.griefer_utils.event.EventListener;
@@ -28,6 +29,11 @@ import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
 import io.netty.buffer.Unpooled;
+import net.labymod.core.LabyModCore;
+import net.labymod.settings.LabyModAddonsGui;
+import net.labymod.utils.ModColor;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.server.S21PacketChunkData;
@@ -42,21 +48,40 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
-import static dev.l3g7.griefer_utils.util.MinecraftUtil.world;
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.*;
 
 @Singleton
 public class KeepChunksLoaded extends Feature {
 
 	private static final Set<ChunkCoordIntPair> forceLoadedChunks = new HashSet<>();
+	private static boolean showingWarning = false;
 
 	@MainElement
 	private final BooleanSetting enabled = new BooleanSetting()
 		.name("Chunks geladen lassen")
 		.icon("chunk")
-		.description("Lässt Chunks nicht entladen.")
-		.callback(b -> {
-			if (b || world() == null)
+		.description("Lässt Chunks nicht entladen.");
+
+	@Override
+	public void init() {
+		super.init();
+		enabled.callback(b -> {
+			if (showingWarning) {
+				if (mc().currentScreen instanceof GuiKCLWarning)
+					showingWarning = false;
+				return;
+			}
+
+			if (b) {
+				if (Constants.OPTIFINE) {
+					showingWarning = true;
+					enabled.set(false);
+					mc().displayGuiScreen(new GuiKCLWarning((LabyModAddonsGui) mc().currentScreen));
+				}
+				return;
+			}
+
+			if (world() == null)
 				return;
 
 			// Unload force loaded chunks
@@ -76,6 +101,7 @@ public class KeepChunksLoaded extends Feature {
 				}
 			}
 		});
+	}
 
 	@EventListener
 	public void onPacketReceive(PacketEvent.PacketReceiveEvent event) {
@@ -125,6 +151,61 @@ public class KeepChunksLoaded extends Feature {
 		}
 
 		return entityLists;
+	}
+
+	private class GuiKCLWarning extends GuiScreen {
+
+		private final LabyModAddonsGui labyModAddonsGui;
+		private GuiButton buttonCancel;
+		private GuiButton buttonActivate;
+
+		private GuiKCLWarning(LabyModAddonsGui labyModAddonsGui) {
+			this.labyModAddonsGui = labyModAddonsGui;
+		}
+
+		@Override
+		public void initGui() {
+			super.initGui();
+			buttonList.add(buttonActivate = new GuiButton(2, width / 2 - 95, height / 2 + 20, 90, 20, "Aktivieren"));
+			buttonList.add(buttonCancel = new GuiButton(3, width / 2 + 5, height / 2 + 20, 90, 20, "Abbrechen"));
+		}
+
+		@Override
+		public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+			super.drawScreen(mouseX, mouseY, partialTicks);
+			labyModAddonsGui.drawScreen(mouseX, mouseY, partialTicks);
+
+			drawUtils().drawIngameBackground();
+
+			GuiScreen gui = mc().currentScreen;
+			drawUtils().drawRectangle(gui.width / 2 - 100 - 2, gui.height / 2 - 50 - 2, gui.width / 2 + 100 + 2, gui.height / 2 + 50 + 2, Integer.MIN_VALUE);
+			drawUtils().drawRectangle(gui.width / 2 - 100, gui.height / 2 - 50, gui.width / 2 + 100, gui.height / 2 + 50, ModColor.toRGB(20, 20, 20, 144));
+			LabyModCore.getMinecraft().drawButton(buttonActivate, mouseX, mouseY);
+			LabyModCore.getMinecraft().drawButton(buttonCancel, mouseX, mouseY);
+			drawUtils().drawCenteredString("§cWarnung", gui.width / 2d, gui.height / 2d - 44);
+			List<String> list = drawUtils().listFormattedStringToWidth("§4Chunks geladen kann zusammen mit OptiFine zu unsichtbaren Entities führen.", 190);
+			int lineY = 0;
+
+			for (String s : list) {
+				drawUtils().drawCenteredString("§4" + s, gui.width / 2d, gui.height / 2d - 30 + lineY);
+				lineY += 10;
+			}
+		}
+
+		protected void actionPerformed(GuiButton button) throws IOException {
+			super.actionPerformed(button);
+			if (button == buttonCancel) {
+				showingWarning = false;
+				mc().displayGuiScreen(labyModAddonsGui);
+			}
+
+			if (button == buttonActivate) {
+				enabled.set(true);
+				mc().displayGuiScreen(labyModAddonsGui);
+			}
+
+		}
+
 	}
 
 }

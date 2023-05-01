6c83de2a3db3b7d207c5ae4d942798417fe0a3ee
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-05-01 20:31:25 +0200

    Added option to search features

--- addon/src/main/java/dev/l3g7/griefer_utils/settings/MainPage.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/settings/MainPage.java
@@ -18,39 +18,58 @@
 
 package dev.l3g7.griefer_utils.settings;
 
+import dev.l3g7.griefer_utils.core.file_provider.FileProvider;
+import dev.l3g7.griefer_utils.core.misc.Constants;
+import dev.l3g7.griefer_utils.core.misc.TickScheduler;
+import dev.l3g7.griefer_utils.core.reflection.Reflection;
+import dev.l3g7.griefer_utils.event.EventListener;
+import dev.l3g7.griefer_utils.event.events.LabyModAddonsGuiInitEvent;
 import dev.l3g7.griefer_utils.features.Category;
 import dev.l3g7.griefer_utils.features.Feature;
-import dev.l3g7.griefer_utils.core.file_provider.FileProvider;
-import dev.l3g7.griefer_utils.util.MinecraftUtil;
-import dev.l3g7.griefer_utils.core.misc.Constants;
 import dev.l3g7.griefer_utils.settings.elements.HeaderSetting;
+import dev.l3g7.griefer_utils.settings.elements.StringSetting;
+import net.labymod.gui.elements.ModTextField;
+import net.labymod.settings.LabyModAddonsGui;
 import net.labymod.settings.elements.SettingsElement;
 import net.minecraft.crash.CrashReport;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
 
 /**
  * The main page of the addon's settings.
  */
 public class MainPage {
 
+	private static final StringSetting filter = new StringSetting()
+		.name("Suche")
+		.icon("magnifying_glass")
+		.callback(MainPage::onSearch);
+
 	public static final List<SettingsElement> settings = new ArrayList<>(Arrays.asList(
 		new HeaderSetting("§r"),
 		new HeaderSetting("§r§e§l" + Constants.ADDON_NAME).scale(1.3),
 		new HeaderSetting("§e§lStartseite").scale(.7),
+		new HeaderSetting("§r").scale(.4).entryHeight(10),
+		filter,
 		new HeaderSetting("§r").scale(.4).entryHeight(10)));
+
+	private static final List<Feature> features = new ArrayList<>();
 
 	static {
 		try {
 			loadFeatures();
 		} catch (Throwable t) {
-			MinecraftUtil.mc().displayCrashReport(new CrashReport("GrieferUtils konnte nicht geladen werden!", t));
+			mc().displayCrashReport(new CrashReport("GrieferUtils konnte nicht geladen werden!", t));
 		}
 	}
 
 	private static void loadFeatures() {
 		// Load features
-		List<Feature> features = new ArrayList<>();
 		FileProvider.getClassesWithSuperClass(Feature.class).forEach(meta -> {
 			if (!meta.isAbstract())
 				features.add(FileProvider.getSingleton(meta.load()));
@@ -69,12 +88,46 @@ public class MainPage {
 
 		settings.add(new HeaderSetting());
 
-		Category.getCategories()
-			.stream()
+		Category.getCategories().stream()
 			.filter(c -> c.getConfigKey() == null)
 			.flatMap(s -> s.getSetting().getSubSettings().getElements().stream())
 			.sorted(Comparator.comparing(SettingsElement::getDisplayName))
 			.forEach(settings::add);
+	}
+
+	private static void onSearch(String query) {
+		TickScheduler.runAfterRenderTicks(() -> {
+			if (!(mc().currentScreen instanceof LabyModAddonsGui))
+				return;
+
+			List<SettingsElement> listedElementsStored = Reflection.get(mc().currentScreen, "tempElementsStored");
+
+			if (query.isEmpty()) {
+				listedElementsStored.clear();
+				listedElementsStored.addAll(settings);
+				return;
+			}
+
+			int startIndex = 6;
+			while (listedElementsStored.size() > startIndex)
+				listedElementsStored.remove(startIndex);
+
+			features.stream()
+				.filter(f -> f.getMainElement().getDisplayName().replaceAll("§.", "").toLowerCase().contains(query.toLowerCase()))
+				.forEach(f -> listedElementsStored.add(f.getMainElement()));
+		}, 1);
+	}
+
+	@EventListener
+	private static void onGuiInit(LabyModAddonsGuiInitEvent event) {
+		filter.getStorage().value = "";
+
+		Reflection.set(filter, "", "currentValue");
+		ModTextField textField = Reflection.get(filter, "textField");
+		textField.setText("");
+		textField.setFocused(false);
+
+		filter.unfocus(0, 0, 0);
 	}
 
 }

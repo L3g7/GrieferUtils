69f31e94e6f87e73d807cbaed60a3c80e6a6c398
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-05-18 18:15:49 +0200

    Removed stuff that shouldn't have been commited

--- addon/src/main/java/dev/l3g7/griefer_utils/features/uncategorized/settings/Settings.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/uncategorized/settings/Settings.java
@@ -18,15 +18,15 @@
 
 package dev.l3g7.griefer_utils.features.uncategorized.settings;
 
+import dev.l3g7.griefer_utils.core.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.misc.Constants;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.features.uncategorized.settings.auto_update.AutoUpdate;
 import dev.l3g7.griefer_utils.features.uncategorized.settings.badges.Badges;
 import dev.l3g7.griefer_utils.features.uncategorized.settings.debug.Debug;
-import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.CategorySetting;
 import dev.l3g7.griefer_utils.settings.elements.HeaderSetting;
-import dev.l3g7.griefer_utils.core.misc.Constants;
 
 @Singleton
 public class Settings extends Feature {
@@ -35,7 +35,7 @@ public class Settings extends Feature {
 	private final CategorySetting element = new CategorySetting()
 		.name("§yEinstellungen")
 		.icon("cog")
-		.subSettings(AutoUpdate.enabled, Changelog.category, Badges.enabled, new HeaderSetting(), Telemetry.category, EasterEggs.button);
+		.subSettings(AutoUpdate.enabled, Changelog.category, Badges.enabled, new HeaderSetting(), Telemetry.category);
 
 	{
 		if (Constants.DEBUG) {

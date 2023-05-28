ca7495df981f7ed5af894905d95e0945759809e8
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-05-29 00:24:35 +0200

    BlockRefill no longer refills with compressed items

--- addon/src/main/java/dev/l3g7/griefer_utils/features/item/inventory_tweaks/tweaks/BlockRefill.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/item/inventory_tweaks/tweaks/BlockRefill.java
@@ -78,6 +78,9 @@ public class BlockRefill extends InventoryTweaks.InventoryTweak {
 			if (!expectedStack.isItemStackDamageable() && expectedStack.getItemDamage() != itemStack.getItemDamage())
 				continue;
 
+			if (itemStack.hasTagCompound() && itemStack.getTagCompound().hasKey("stackSize"))
+				continue;
+
 			// Compare NBT
 			if (!Objects.equals(expectedStack.getTagCompound(), itemStack.getTagCompound()))
 				continue;

8fe87ccc37cdd74907a73fdcb1c1f41bbbc90f87
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-06-28 23:51:59 +0200

    Fixed recursion when calling OnStartupComplete

--- addon/src/main/java/dev/l3g7/griefer_utils/event/AnnotationEventHandler.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/event/AnnotationEventHandler.java
@@ -98,8 +98,8 @@ public class AnnotationEventHandler implements Opcodes {
 			return;
 
 		// Call all methods annotated with @OnStartupComplete
-		triggerEvent(OnStartupComplete.class);
 		startupComplete = true;
+		triggerEvent(OnStartupComplete.class);
 	}
 
 	/**

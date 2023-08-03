fb25774972fde3bcb18a16310e415521bde716ff
L3g7 <contact@l3g7.dev>
2023-08-03 17:43:26 +0200

    Rewrite PlotGreetingWarning

--- addon/src/main/java/dev/l3g7/griefer_utils/features/chat/PlotGreetingWarning.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/chat/PlotGreetingWarning.java
@@ -21,12 +21,15 @@ package dev.l3g7.griefer_utils.features.chat;
 import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.core.reflection.Reflection;
 import dev.l3g7.griefer_utils.event.EventListener;
-import dev.l3g7.griefer_utils.event.events.MessageEvent.MessageModifyEvent;
+import dev.l3g7.griefer_utils.event.events.network.PacketEvent;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import dev.l3g7.griefer_utils.settings.elements.NumberSetting;
 import io.netty.channel.ChannelHandler;
 import net.minecraft.event.HoverEvent;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.util.ChatAllowedCharacters;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
 
@@ -36,45 +39,73 @@ import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
 public class PlotGreetingWarning extends Feature {
 
 	private static final String GREETING_PATTERN = "^§r§8\\[§r§6GrieferGames§r§8] §r§8\\[§r§6-?\\d+;-?\\d+§r§8] ";
-	private long lastReadTime = -1;
+	private long lastGreetingReadTime = 0;
+
+	private final NumberSetting maxTimeDifference = new NumberSetting()
+		.name("Max. Zeitabstand (µs)")
+		.description("Wie viel Zeit (in Mikrosekunden) maximal zwischen zwei Plot-Greeting-Nachrichten vergehen darf.")
+		.icon("hourglass")
+		.defaultValue(256);
 
 	@MainElement
 	private final BooleanSetting enabled = new BooleanSetting()
-		.name("Plot-Greeting-Warnung")
+		.name("Warnung bei Plot-Greeting")
 		.description("Fügt einen Tag zu Nachrichten hinzu, die von einem Plot-Greeting kommen.")
-		.icon("labymod:buttons/exclamation_mark");
+		.icon("labymod:buttons/exclamation_mark")
+		.subSettings(maxTimeDifference);
 
 	@EventListener
-	public void onMessageModify(MessageModifyEvent event) {
-		if (event.original.getFormattedText().matches(GREETING_PATTERN + ".*")) {
-			lastReadTime = getLastReadTime();
+	public void onMessageModify(PacketEvent.PacketReceiveEvent event) {
+		if (!(event.packet instanceof S02PacketChat))
+			return;
 
-			// Add warning if text follows
-			if (!event.original.getFormattedText().matches(GREETING_PATTERN + "§r$"))
-				addWarning(event.message);
+		long lastReadTime = getLastReadTime();
+		IChatComponent message = ((S02PacketChat) event.packet).getChatComponent();
+
+		// Check if message was begin of greeting
+		if (message.getFormattedText().matches(GREETING_PATTERN + ".*")) {
+			lastGreetingReadTime = lastReadTime;
+
+			// Mark plot greeting if it has additional text
+			if (!message.getFormattedText().matches(GREETING_PATTERN + "§r$"))
+				addWarning(message, true);
 
 			return;
 		}
 
-		if (lastReadTime == -1)
-			return;
+		// Guess whether message was part of greeting
 
-		if (lastReadTime != getLastReadTime()) {
-			lastReadTime = -1;
-			return;
+		// Check if message was within specified time
+		if (lastGreetingReadTime != 0 && lastReadTime - lastGreetingReadTime <= maxTimeDifference.get() * 1000) {
+
+			// Check if message is nested
+			if (message.getSiblings().stream().anyMatch(c -> !c.getSiblings().isEmpty()))
+				return;
+
+			// Check if message contains events
+			if (message.getSiblings().stream().anyMatch(v -> v.getChatStyle().getChatHoverEvent() != null || v.getChatStyle().getChatClickEvent() != null))
+				return;
+
+			// Check if message contains illegal characters
+			String textMessage = message.getUnformattedText();
+			if (!textMessage.equals(ChatAllowedCharacters.filterAllowedCharacters(textMessage)))
+				return;
+
+			// Message seems to be part of a greeting
+			addWarning(message, false);
 		}
-
-		if (!event.original.getFormattedText().equals("§r§8[§r§6GrieferGames§r§8] §r§aDu wurdest zum Grundstück teleportiert.§r"))
-			addWarning(event.message);
 	}
 
-	private void addWarning(IChatComponent message) {
+	private void addWarning(IChatComponent message, boolean sure) {
 		IChatComponent icc = new ChatComponentText("§e [⚠]");
-		icc.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("§eDiese Nachricht stammt von einem Plot-Gretting.")));
+		icc.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("§eDiese Nachricht stammt " + (sure ? "" : "wahrscheinlich ") + "von einem Plot-Greeting.")));
 		message.appendSibling(icc);
 	}
 
 	private long getLastReadTime() {
+		if (mc().getNetHandler() == null)
+			return 0;
+
 		ChannelHandler timeoutHandler = mc().getNetHandler().getNetworkManager().channel().pipeline().get("timeout");
 		return Reflection.get(timeoutHandler, "lastReadTime");
 	}

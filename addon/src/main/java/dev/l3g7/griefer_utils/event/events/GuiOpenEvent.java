f3f3fb07f53ff89a168a1b33512be2ef40e46947
L3g73 <103503837+L3g73@users.noreply.github.com>
2023-09-10 01:36:28 +0200

    Fix canceling GuiOpenEvents not doing anything

--- addon/src/main/java/dev/l3g7/griefer_utils/event/events/GuiOpenEvent.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/event/events/GuiOpenEvent.java
@@ -24,13 +24,18 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiGameOver;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiWinGame;
 import net.minecraft.client.multiplayer.WorldClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class GuiOpenEvent<G extends GuiScreen> extends TypedEvent<GuiOpenEvent<G>> {
+
+	public static final GuiScreen CANCEL_INDICATOR = new GuiWinGame();
 
 	public G gui;
 
@@ -56,8 +61,15 @@ public class GuiOpenEvent<G extends GuiScreen> extends TypedEvent<GuiOpenEvent<G
 					screen = new GuiGameOver();
 			}
 
-			return new GuiOpenEvent<>(screen).fire().gui;
+			GuiOpenEvent<?> event = new GuiOpenEvent<>(screen).fire();
+			return event.isCanceled() ? CANCEL_INDICATOR : event.gui;
 	    }
+
+		@Inject(method = "displayGuiScreen", at = @At("HEAD"), cancellable = true)
+		public void injectDisplayGuiScreen(GuiScreen guiScreenIn, CallbackInfo ci) {
+			if (guiScreenIn == CANCEL_INDICATOR)
+				ci.cancel();
+		}
 
 	}
 

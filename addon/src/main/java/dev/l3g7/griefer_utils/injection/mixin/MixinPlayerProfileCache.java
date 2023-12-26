7a303b2182691017211e22f34517eb59e921996b
L3g7 <contact@l3g7.dev>
2023-12-26 03:31:50 +0100

    Fix concurrent modification in PlayerProfileCache

--- addon/src/main/java/dev/l3g7/griefer_utils/injection/mixin/MixinPlayerProfileCache.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/injection/mixin/MixinPlayerProfileCache.java
@@ -19,12 +19,15 @@
 package dev.l3g7.griefer_utils.injection.mixin;
 
 import com.mojang.authlib.GameProfile;
+import dev.l3g7.griefer_utils.core.reflection.Reflection;
 import net.minecraft.server.management.PlayerProfileCache;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -37,7 +40,28 @@ public abstract class MixinPlayerProfileCache {
 	private LinkedList<GameProfile> gameProfiles;
 
 	/**
-	 * Fix concurrent modification while saving the cache.
+	 * Fix concurrent modification while accessing the cache.
+	 */
+	@Inject(method = "load", at = @At(value = "HEAD"))
+	public void injectLoad(CallbackInfo ci) {
+		Reflection.set(this, new LinkedList<GameProfile>() {
+			@Override
+			public boolean remove(Object o) {
+				synchronized (this) {
+					return super.remove(o);
+				}
+			}
+			@Override
+			public void addFirst(GameProfile gameProfile) {
+				synchronized (this) {
+					super.addFirst(gameProfile);
+				}
+			}
+		}, "gameProfiles");
+	}
+
+	/**
+	 * Fix concurrent modification while accessing the cache.
 	 */
 	@Redirect(method = "save", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerProfileCache;getEntriesWithLimit(I)Ljava/util/List;"))
 	public List<?> redirectIsInGame(PlayerProfileCache instance, int limitSize) {

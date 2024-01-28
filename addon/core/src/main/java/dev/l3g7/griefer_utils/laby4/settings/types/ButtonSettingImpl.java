99971bf1844b32fe94246bb7378396593cd3eeeb
L3g7 <contact@l3g7.dev>
2024-01-28 02:33:10 +0100

    Fix rendering of ButtonSettings

--- addon/core/src/main/java/dev/l3g7/griefer_utils/laby4/settings/types/ButtonSettingImpl.java
+++ addon/core/src/main/java/dev/l3g7/griefer_utils/laby4/settings/types/ButtonSettingImpl.java
@@ -8,13 +8,18 @@
 package dev.l3g7.griefer_utils.laby4.settings.types;
 
 import com.google.gson.JsonNull;
+import dev.l3g7.griefer_utils.api.event.event_bus.EventListener;
+import dev.l3g7.griefer_utils.api.event.event_bus.EventRegisterer;
+import dev.l3g7.griefer_utils.laby4.events.SettingActivityInitEvent;
 import dev.l3g7.griefer_utils.laby4.settings.AbstractSettingImpl;
 import dev.l3g7.griefer_utils.laby4.settings.SettingsImpl;
 import dev.l3g7.griefer_utils.settings.types.ButtonSetting;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.screen.widget.Widget;
+import net.labymod.api.client.gui.screen.widget.widgets.activity.settings.SettingWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.input.ButtonWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.FlexibleContentWidget;
 
 public class ButtonSettingImpl extends AbstractSettingImpl<ButtonSetting, Object> implements ButtonSetting {
 
@@ -22,21 +27,47 @@ public class ButtonSettingImpl extends AbstractSettingImpl<ButtonSetting, Object
 
 	public ButtonSettingImpl() {
 		super(e -> JsonNull.INSTANCE, e -> NULL, NULL);
+		EventRegisterer.register(this);
 	}
 
 	@Override
 	protected Widget[] createWidgets() {
-		return new Widget[]{
-			buttonIcon == null ?
-				ButtonWidget.component(Component.text("§cNo icon"), null, () -> set(null)) :
-				ButtonWidget.icon(buttonIcon, () -> set(null))
-		};
+		return null;
 	}
 
 	@Override
 	public ButtonSetting buttonIcon(Object icon) {
 		buttonIcon = SettingsImpl.buildIcon(icon);
 		return this;
+	}
+
+	@Override
+	public boolean hasAdvancedButton() {
+		return true;
+	}
+
+	@EventListener
+	private void onInit(SettingActivityInitEvent event) {
+		if (event.holder() != parent)
+			return;
+
+		for (Widget w : event.settings().getChildren()) {
+			if (w instanceof SettingWidget s && s.setting() == this) {
+				SettingsImpl.hookChildAdd(s, e -> {
+					if (e.childWidget() instanceof FlexibleContentWidget content) {
+						ButtonWidget btn = buttonIcon == null ?
+							ButtonWidget.component(Component.text("§cNo icon"), null, () -> set(null)) :
+							ButtonWidget.icon(buttonIcon, () -> set(null));
+
+						btn.addId("advanced-button"); // required so LSS is applied
+						content.removeChild("advanced-button");
+						content.addContent(btn);
+					}
+				});
+				break;
+			}
+		}
+
 	}
 
 }

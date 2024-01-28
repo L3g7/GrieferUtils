dbd30ef19b32d11b56a03079407f98322f67e28e
L3g73 <103503837+L3g73@users.noreply.github.com>
2024-01-28 18:48:41 +0100

    Implement ChatMessageLogEvent

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/events/ChatMessageLogEvent.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/events/ChatMessageLogEvent.java
@@ -7,7 +7,16 @@
 
 package dev.l3g7.griefer_utils.v1_8_9.events;
 
+import dev.l3g7.griefer_utils.api.bridges.Bridge.ExclusiveTo;
 import dev.l3g7.griefer_utils.api.event.event_bus.Event;
+import net.labymod.core_implementation.mc18.gui.GuiChatAdapter;
+import net.minecraft.client.gui.GuiNewChat;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static dev.l3g7.griefer_utils.api.bridges.Bridge.Version.*;
 
 public class ChatMessageLogEvent extends Event {
 
@@ -17,18 +26,33 @@ public class ChatMessageLogEvent extends Event {
 		this.message = message;
 	}
 
-	/*
-	// TODO: @Mixin(value = GuiChatAdapter.class, remap = false)
+	public static void tryLogging(Logger logger, String message) {
+		ChatMessageLogEvent event = new ChatMessageLogEvent(message);
+
+		if (!event.fire().isCanceled())
+			logger.info(event.message);
+	}
+
+	@Mixin(GuiNewChat.class)
+	@ExclusiveTo({LABY_4, ANY_MINECRAFT})
+	private static class MixinGuiNewChat {
+
+		@Redirect(method = "printChatMessageWithOptionalDeletion", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V"))
+		public void log(Logger logger, String message) {
+			tryLogging(logger, message);
+		}
+
+	}
+
+	@Mixin(value = GuiChatAdapter.class, remap = false)
+	@ExclusiveTo({LABY_3, ANY_MINECRAFT})
 	private static class MixinGuiChatAdapter {
 
 		@Redirect(method = "setChatLine", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V"))
 		public void log(Logger logger, String message) {
-			ChatMessageLogEvent event = new ChatMessageLogEvent(message);
-
-			if (!event.fire().isCanceled())
-				logger.info(event.message);
+			tryLogging(logger, message);
 		}
 
-	}*/
+	}
 
 }

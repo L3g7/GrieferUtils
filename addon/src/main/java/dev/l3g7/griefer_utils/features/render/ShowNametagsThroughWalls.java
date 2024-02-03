421d52f50619ddf05f942548d70b0c9648614b6c
L3g73 <103503837+L3g73@users.noreply.github.com>
2024-02-03 02:58:22 +0100

    Fix ShowNametagsThroughWalls' custom rendering being used for unaffected entities

--- addon/src/main/java/dev/l3g7/griefer_utils/features/render/ShowNametagsThroughWalls.java
+++ addon/src/main/java/dev/l3g7/griefer_utils/features/render/ShowNametagsThroughWalls.java
@@ -23,18 +23,24 @@ import dev.l3g7.griefer_utils.core.file_provider.Singleton;
 import dev.l3g7.griefer_utils.features.Feature;
 import dev.l3g7.griefer_utils.settings.ElementBuilder.MainElement;
 import dev.l3g7.griefer_utils.settings.elements.BooleanSetting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.mc;
+import static dev.l3g7.griefer_utils.util.MinecraftUtil.player;
 
 @Singleton
 public class ShowNametagsThroughWalls extends Feature {
@@ -44,6 +50,17 @@ public class ShowNametagsThroughWalls extends Feature {
 		.name("Nametags durch Wände anzeigen")
 		.description("Zeigt Nametags auch durch Wände an.\n(Funktioniert nicht bei Spielern)")
 		.icon("yellow_name");
+
+	/**
+	 * The canRenderName method of {@link RendererLivingEntity}, assuming the entity isn't a player
+	 */
+	public static boolean baseCanRenderName(Entity entity) {
+		return Minecraft.isGuiEnabled()
+			&& entity != mc().getRenderManager().livingPlayer
+			&& !entity.isInvisibleToPlayer(player())
+			&& entity.riddenByEntity == null
+			&& entity.hasCustomName();
+	}
 
 	@Mixin(RenderLiving.class)
 	private static abstract class MixinRenderLiving extends RendererLivingEntity<EntityLiving> {
@@ -55,7 +72,7 @@ public class ShowNametagsThroughWalls extends Feature {
 		@Inject(method = "canRenderName(Lnet/minecraft/entity/EntityLiving;)Z", at = @At("RETURN"), cancellable = true)
 	    private void injectCanRenderName(EntityLiving entity, CallbackInfoReturnable<Boolean> cir) {
 	    	if (FileProvider.getSingleton(ShowNametagsThroughWalls.class).isEnabled() && !cir.getReturnValueZ())
-				cir.setReturnValue(super.canRenderName(entity) && entity.hasCustomName());
+				cir.setReturnValue(baseCanRenderName(entity) && entity.hasCustomName());
 	    }
 
 	}
@@ -63,10 +80,17 @@ public class ShowNametagsThroughWalls extends Feature {
 	@Mixin(Render.class)
 	private static class MixinRender {
 
+		private static boolean renderingLivingEntity;
+
+		@Inject(method = "renderLivingLabel", at = @At("HEAD"))
+		private void injectRenderLivingLabel(Entity entityIn, String str, double x, double y, double z, int maxDistance, CallbackInfo ci) {
+			renderingLivingEntity = entityIn instanceof EntityLiving;
+		}
+
 		@Redirect(method = "renderLivingLabel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;tryBlendFuncSeparate(IIII)V"))
 		private void redirectTryBlendFuncSeparate(int srcFactor, int dstFactor, int srcFactorAlpha, int dstFactorAlpha) {
-			boolean enabled = FileProvider.getSingleton(ShowNametagsThroughWalls.class).isEnabled();
-			GlStateManager.tryBlendFuncSeparate(enabled ? dstFactor : srcFactor, enabled ? srcFactor : dstFactor, srcFactorAlpha, dstFactorAlpha);
+			boolean swap = renderingLivingEntity && FileProvider.getSingleton(ShowNametagsThroughWalls.class).isEnabled();
+			GlStateManager.tryBlendFuncSeparate(swap ? dstFactor : srcFactor, swap ? srcFactor : dstFactor, srcFactorAlpha, dstFactorAlpha);
 		}
 
 	}

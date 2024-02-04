333fdcdb534554b355e625df08062d10bb78479a
L3g7 <contact@l3g7.dev>
2024-02-04 21:53:13 +0100

    Complete ImageSelection

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/misc/gui/elements/ImageSelection.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/misc/gui/elements/ImageSelection.java
@@ -1,5 +1,6 @@
 package dev.l3g7.griefer_utils.v1_8_9.misc.gui.elements;
 
+import com.sun.jna.*;
 import dev.l3g7.griefer_utils.api.bridges.LabyBridge;
 import dev.l3g7.griefer_utils.v1_8_9.misc.gui.elements.laby_polyfills.DrawUtils;
 import dev.l3g7.griefer_utils.v1_8_9.misc.gui.elements.laby_polyfills.ModTextField;
@@ -7,9 +8,15 @@ import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.util.ResourceLocation;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import static dev.l3g7.griefer_utils.v1_8_9.util.MinecraftUtil.mc;
 
@@ -30,7 +37,7 @@ public class ImageSelection extends ModTextField implements Drawable, Clickable 
 		super(0, mc().fontRendererObj, 0, 0, 0, 20);
 		this.button = new Button("Auswählen")
 			.size(60, 20)
-			.callback(() -> {}/*TODO: FileSelectionDialog.chooseFile(this::processChosenFile, "Bild", ImageIO.getReaderFileSuffixes())*/);
+			.callback(() -> FileSelectionDialog.chooseFile(this::processChosenFile, "Bild", ImageIO.getReaderFileSuffixes()));
 
 		this.label = label;
 		setMaxStringLength(Integer.MAX_VALUE);
@@ -126,6 +133,118 @@ public class ImageSelection extends ModTextField implements Drawable, Clickable 
 	public void mousePressed(int mouseX, int mouseY, int mouseButton) {
 		button.mousePressed(mouseX, mouseY, mouseButton);
 //		super.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+
+	public static class FileSelectionDialog {
+
+		static {
+			if (Platform.isWindows())
+				Native.register("comdlg32");
+		}
+
+		public static native boolean GetOpenFileNameW(OpenFileName params);
+
+		public static native int CommDlgExtendedError();
+
+		public static void chooseFile(Consumer<File> fileConsumer, String filterName, String... allowedFileTypes) {
+			new Thread(() -> {
+				if (Platform.isWindows())
+					windowsFileChooser(fileConsumer, filterName, allowedFileTypes);
+				else
+					swingFileChooser(fileConsumer, filterName, allowedFileTypes);
+			}).start();
+
+		}
+
+		private static void swingFileChooser(Consumer<File> fileConsumer, String filterName, String[] allowedFileTypes) {
+			JFileChooser fc = new JFileChooser((File) null);
+			fc.setMultiSelectionEnabled(false);
+			fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+			if (filterName != null) {
+				fc.addChoosableFileFilter(new FileFilter() {
+					@Override
+					public boolean accept(File f) {
+						if (f.isDirectory())
+							return true;
+
+						for (String allowedFileType : allowedFileTypes)
+							if (f.getName().endsWith("." + allowedFileType))
+								return true;
+
+						return false;
+					}
+
+					@Override
+					public String getDescription() {
+						return filterName;
+					}
+				});
+			}
+
+			fileConsumer.accept(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION ? fc.getSelectedFile() : null);
+		}
+
+		private static void windowsFileChooser(Consumer<File> fileConsumer, String filterName, String[] allowedFileTypes) {
+			OpenFileName params = new OpenFileName();
+			params.lpstrFile = new Memory(1041);
+			params.lpstrFile.clear(1041);
+			params.nMaxFile = 260;
+			if (filterName != null)
+				params.lpstrFilter = new WString(filterName + "\0" + Arrays.stream(allowedFileTypes).map(f -> "*." + f).collect(Collectors.joining(";")) + "\0\0");
+
+
+			if (GetOpenFileNameW(params)) {
+				fileConsumer.accept(new File(params.lpstrFile.getWideString(0)));
+				return;
+			}
+
+			int error = CommDlgExtendedError();
+			if (error != 0) // Selection was aborted by the user
+				System.err.println("GetOpenFileName failed with error " + error);
+
+			fileConsumer.accept(null);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public static class OpenFileName extends Structure {
+
+		public OpenFileName() {
+			super();
+			lStructSize = size();
+		}
+
+		public int lStructSize;
+		public Pointer hwndOwner;
+		public Pointer hInstance;
+		public WString lpstrFilter;
+		public WString lpstrCustomFilter;
+		public int nMaxCustFilter;
+		public int nFilterIndex;
+		public Pointer lpstrFile;
+		public int nMaxFile;
+		public String lpstrDialogTitle;
+		public int nMaxDialogTitle;
+		public WString lpstrInitialDir;
+		public WString lpstrTitle;
+		public int Flags;
+		public short nFileOffset;
+		public short nFileExtension;
+		public String lpstrDefExt;
+		public Pointer lCustData;
+		public Pointer lpfnHook;
+		public Pointer lpTemplateName;
+
+		@Override
+		protected List<String> getFieldOrder() {
+			return Arrays.asList("lStructSize",
+				"hwndOwner", "hInstance", "lpstrFilter", "lpstrCustomFilter"
+				, "nMaxCustFilter", "nFilterIndex", "lpstrFile", "nMaxFile"
+				, "lpstrDialogTitle", "nMaxDialogTitle", "lpstrInitialDir", "lpstrTitle"
+				, "Flags", "nFileOffset", "nFileExtension", "lpstrDefExt"
+				, "lCustData", "lpfnHook", "lpTemplateName");
+		}
 	}
 
 }

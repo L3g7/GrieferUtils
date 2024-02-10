2442ab12e8208a0ca1dab54ebe99669ffde4aa3f
L3g7 <contact@l3g7.dev>
2024-02-10 04:33:44 +0100

    Fix LabyMod upgrading to unstable versions

--- addon/core/build.gradle.kts
+++ addon/core/build.gradle.kts
@@ -9,7 +9,12 @@ dependencies {
 
     compileOnly(fileTree("../libs"))
     implementation("net.minecraft:launchwrapper:1.12")
-    implementation("net.labymod.labymod4:core:+")
+
+    // Required because the core package actually doesn't include LabyMod 4's core, only its api.
+    // I can't just use the latest version as LabyMod sometimes publishes unstable versions with breaking changes.
+    // I can't get it from game-runner as game-runner is dependent on core and its build script model must be fully prepared to get its dependencies.
+    // I hope that's just a temporary fix
+    implementation("net.labymod.labymod4:core:4.1.17-internal-48ca5e2b")
 }
 
 labyModProcessor {

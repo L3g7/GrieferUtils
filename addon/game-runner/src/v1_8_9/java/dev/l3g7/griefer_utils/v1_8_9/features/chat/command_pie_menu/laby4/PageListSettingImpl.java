2cd3017b35b9f1ee6cf2af495bc42e36e86c6d5a
L3g7 <contact@l3g7.dev>
2024-05-04 00:32:25 +0200

    Merge command pie menu

---
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/features/chat/command_pie_menu/laby4/PageListSettingImpl.java
@@ -0,0 +1,193 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
+package dev.l3g7.griefer_utils.v1_8_9.features.chat.command_pie_menu.laby4;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import dev.l3g7.griefer_utils.api.bridges.Bridge;
+import dev.l3g7.griefer_utils.api.bridges.Bridge.ExclusiveTo;
+import dev.l3g7.griefer_utils.api.event.event_bus.EventListener;
+import dev.l3g7.griefer_utils.api.event.event_bus.EventRegisterer;
+import dev.l3g7.griefer_utils.laby4.events.SettingActivityInitEvent;
+import dev.l3g7.griefer_utils.laby4.settings.BaseSettingImpl;
+import dev.l3g7.griefer_utils.laby4.settings.SettingsImpl;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.client.gui.screen.widget.Widget;
+import net.labymod.api.client.gui.screen.widget.widgets.activity.settings.SettingWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.input.ButtonWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.FlexibleContentWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.entry.FlexibleContentEntry;
+import net.labymod.api.client.gui.screen.widget.widgets.renderer.IconWidget;
+import net.labymod.api.configuration.settings.Setting;
+import net.labymod.api.configuration.settings.accessor.impl.ConfigPropertySettingAccessor;
+import net.labymod.api.configuration.settings.type.list.ListSetting;
+import net.labymod.api.configuration.settings.type.list.ListSettingEntry;
+import net.labymod.api.util.KeyValue;
+import net.minecraft.init.Items;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static dev.l3g7.griefer_utils.api.bridges.Bridge.Version.LABY_4;
+import static dev.l3g7.griefer_utils.api.reflection.Reflection.c;
+import static net.labymod.api.Textures.SpriteCommon.X;
+
+// NOTE: cleanup? merge?
+
+@Bridge
+@ExclusiveTo(LABY_4)
+public class PageListSettingImpl extends ListSetting implements PageListSetting, BaseSettingImpl<PageListSetting, List<Page>> {
+
+	private final ExtendedStorage<List<Page>> storage;
+
+	public PageListSettingImpl() {
+		this(new ExtendedStorage<>(v -> {
+			JsonArray pages = new JsonArray();
+			for (PageConfig entry : v) {
+				JsonObject obj = new JsonObject();
+				obj.addProperty("name", entry.name.get());
+				obj.add("entries", entry.entries.storage.encodeFunc.apply(entry.entries.get()));
+				pages.add(obj);
+			}
+			return pages;
+		}, entries -> {
+			List<PageConfig> v = new ArrayList<>();
+			for (JsonElement elem : entries.getAsJsonArray()) {
+				JsonObject page = elem.getAsJsonObject();
+				PageConfig cfg = new PageConfig(page.get("name").getAsString());
+				cfg.entries.set(cfg.entries.storage.decodeFunc.apply(page.get("entries")));
+				v.add(cfg);
+			}
+			return v;
+		}, new ArrayList<>()));
+		EventRegisterer.register(this);
+	}
+
+	private PageListSettingImpl(ExtendedStorage<List<PageConfig>> storage) {
+		super(UUID.randomUUID().toString(), null, null, new String[0], null, false, null, (byte) -127,
+			new ConfigPropertySettingAccessor(null, null, null, null) {
+				@Override
+				public <T> T get() {
+					return c(storage.value == null ? storage.fallbackValue : storage.value);
+				}
+
+				@Override
+				public <T> void set(T value) {
+					List<PageConfig> list = get();
+					ArrayList<PageConfig> val = c(value);
+					list.clear();
+					list.addAll(val);
+				}
+
+				@Override
+				public Type getGenericType() {
+					return new ParameterizedType() {
+						public Type[] getActualTypeArguments() {return new Type[]{PageConfig.class};}
+
+						public Type getRawType() {return null;}
+
+						public Type getOwnerType() {return null;}
+					};
+				}
+			}
+		);
+
+		this.storage = c(storage);
+		for (PageConfig page : getTyped())
+			page.create(this);
+	}
+
+	private List<PageConfig> getTyped() {
+		return c(get());
+	}
+
+	@Override
+	public List<KeyValue<Setting>> getElements() {
+		List<KeyValue<Setting>> list = new ArrayList<>();
+
+		List<PageConfig> configs = getTyped();
+		for (int i = 0; i < configs.size(); ++i) {
+			PageConfig config = configs.get(i);
+			if (config.isInvalid()) {
+				configs.remove(i--);
+				notifyChange();
+			} else {
+				ListSettingEntry entry = new ListSettingEntry(this, config.entryDisplayName(), i);
+				entry.addSettings(config);
+				list.add(new KeyValue<>(entry.getId(), entry));
+			}
+		}
+
+		return list;
+	}
+
+	@Override
+	public ListSettingEntry createNew() {
+		PageConfig config = new PageConfig("Seite " + (get().size() + 1));
+		config.create(this);
+		get().add(config);
+		notifyChange();
+
+		ListSettingEntry entry = new ListSettingEntry(this, config.entryDisplayName(), get().size() - 1);
+		entry.addSettings(config);
+		return entry;
+	}
+
+	@EventListener
+	private void onInit(SettingActivityInitEvent event) {
+		if (event.holder() != this)
+			return;
+
+		// Update entry widgets
+		for (Widget w : event.settings().getChildren()) {
+			if (w instanceof SettingWidget s && s.setting() instanceof ListSettingEntry entry) {
+				SettingsImpl.hookChildAdd(s, e -> {
+					if (e.childWidget() instanceof FlexibleContentWidget content) {
+						// Fix icon
+						IconWidget widget = new IconWidget(SettingsImpl.buildIcon(Items.map));
+						widget.addId("setting-icon");
+						content.addChild(0, new FlexibleContentEntry(widget, false));
+						widget.initialize(content);
+
+						// Update button icons
+						ButtonWidget btn = (ButtonWidget) content.getChild("advanced-button").childWidget();
+						btn.updateIcon(SettingsImpl.buildIcon("pencil_vec")); // NOTE: use original icons?
+						content.removeChild("delete-button");
+
+						content.addContent(ButtonWidget.icon(X, () -> {
+							get().remove(entry.listIndex());
+							notifyChange();
+							event.activity.reload();
+						}).addId("delete-button"));
+					}
+				});
+			}
+		}
+	}
+
+	@Override
+	public Component displayName() {
+		return Component.text(name());
+	}
+
+	@Override
+	public Icon getIcon() {
+		return storage.icon;
+	}
+
+	@Override
+	public ExtendedStorage<List<Page>> getStorage() {
+		return storage;
+	}
+
+}
--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/features/chat/command_pie_menu/laby4/PieMenu.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/v1_8_9/features/chat/command_pie_menu/laby4/PieMenu.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  */
 
-package dev.l3g7.griefer_utils.v1_8_9.features.chat.command_pie_menu;
+package dev.l3g7.griefer_utils.v1_8_9.features.chat.command_pie_menu.laby4;
 
 import dev.l3g7.griefer_utils.v1_8_9.events.MessageEvent;
 import org.apache.commons.lang3.tuple.Pair;
@@ -17,23 +17,23 @@ import static dev.l3g7.griefer_utils.v1_8_9.util.MinecraftUtil.player;
 
 public class PieMenu extends dev.l3g7.griefer_utils.v1_8_9.misc.gui.guis.PieMenu {
 
-	public void open(boolean animation, CommandPieMenu.PageListSetting pages) { // NOTE: refactor (merge to CommandPieMenu?)
+	public void open(boolean animation, PageListSetting pages) { // NOTE: refactor (merge to CommandPieMenu?)
 		List<Pair<String, List<Pair<String, Runnable>>>> allPages = new ArrayList<>();
 
-		for (CommandPieMenu.PageConfig page : pages.get()) {
+		for (Page page : pages.get()) {
 			List<Pair<String, Runnable>> entries = new ArrayList<>();
 
-			for (CommandPieMenu.EntryConfig entry : page.entries.get()) {
-				if (!entry.citybuild.get().isOnCb())
+			for (Page.Entry entry : page.entries()) {
+				if (!entry.citybuild().isOnCb())
 					continue;
 
-				entries.add(Pair.of(entry.name.get().replace('&', '§'), () -> {
-					if (!MessageEvent.MessageSendEvent.post(entry.command.get()))
-						player().sendChatMessage(entry.command.get());
+				entries.add(Pair.of(entry.name().replace('&', '§'), () -> {
+					if (!MessageEvent.MessageSendEvent.post(entry.command()))
+						player().sendChatMessage(entry.command());
 				}));
 			}
 
-			allPages.add(Pair.of(page.name.get().replace('&', '§'), entries));
+			allPages.add(Pair.of(page.name().replace('&', '§'), entries));
 		}
 
 		open(animation, allPages);

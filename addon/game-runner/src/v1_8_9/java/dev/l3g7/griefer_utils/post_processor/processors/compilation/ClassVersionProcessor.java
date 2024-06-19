868c9c74f8585e5eda972c74d3b7cb0630a0d506
L3g7 <contact@l3g7.dev>
2024-06-19 19:00:23 +0200

    Update bootstrap classes

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/post_processor/processors/compilation/ClassVersionProcessor.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/post_processor/processors/compilation/ClassVersionProcessor.java
@@ -28,6 +28,7 @@ public class ClassVersionProcessor extends CompilationPostProcessor {
 
 	private static final Predicate<Path> BOOTSTRAP_CLASSES =
 		p -> p.startsWith("dev/l3g7/griefer_utils/labymod/laby3/PreStart.class")
+			|| p.startsWith("dev/l3g7/griefer_utils/core/auto_update/UpdateImpl.class")
 			|| p.startsWith(POST_PROCESSOR_FOLDER + "/processors/runtime/transpiler/")
 			|| p.startsWith(POST_PROCESSOR_FOLDER + "/processors/RuntimePostProcessor.class");
 

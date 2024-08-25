69ca593073e9f42f6a78b4f93e3812665b4ee7a8
L3g7 <contact@l3g7.dev>
2024-08-25 16:06:30 +0200

    Merge OrbBalance

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/widgets/orb_stats/OrbBalance.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/widgets/orb_stats/OrbBalance.java
@@ -5,24 +5,21 @@
  * you may not use this file except in compliance with the License.
  */
 
-package dev.l3g7.griefer_utils.features.modules.laby4.orb_stats;
+package dev.l3g7.griefer_utils.features.widgets.orb_stats;
 
 import com.google.gson.JsonPrimitive;
-import dev.l3g7.griefer_utils.core.api.bridges.Bridge;
-import dev.l3g7.griefer_utils.core.api.bridges.Bridge.ExclusiveTo;
 import dev.l3g7.griefer_utils.core.api.event_bus.EventListener;
 import dev.l3g7.griefer_utils.core.api.file_provider.Singleton;
 import dev.l3g7.griefer_utils.core.api.misc.config.Config;
 import dev.l3g7.griefer_utils.core.api.reflection.Reflection;
-import dev.l3g7.griefer_utils.features.Feature.MainElement;
-import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
 import dev.l3g7.griefer_utils.core.events.MessageEvent.MessageReceiveEvent;
-import dev.l3g7.griefer_utils.core.events.TickEvent;
+import dev.l3g7.griefer_utils.core.events.TickEvent.ClientTickEvent;
 import dev.l3g7.griefer_utils.core.events.network.ServerEvent.GrieferGamesJoinEvent;
-import dev.l3g7.griefer_utils.features.modules.Laby4Module;
-import dev.l3g7.griefer_utils.features.modules.TempOrbBalanceBridge;
 import dev.l3g7.griefer_utils.core.misc.ServerCheck;
+import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
 import dev.l3g7.griefer_utils.core.util.ItemUtil;
+import dev.l3g7.griefer_utils.features.Feature.MainElement;
+import dev.l3g7.griefer_utils.features.widgets.SimpleWidget;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemSkull;
@@ -34,13 +31,10 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static dev.l3g7.griefer_utils.core.api.bridges.Bridge.Version.LABY_4;
 import static dev.l3g7.griefer_utils.core.util.MinecraftUtil.mc;
 
-@Bridge
 @Singleton
-@ExclusiveTo(LABY_4)
-public class OrbBalance extends Laby4Module implements TempOrbBalanceBridge {
+public class OrbBalance extends SimpleWidget {
 
 	private static final Pattern SKULL_PATTERN = Pattern.compile("^§7Du besitzt aktuell §e(?<orbs>[\\d.]+) Orbs§7\\.$");
 	private static final Pattern BUY_PATTERN = Pattern.compile("^\\[GrieferGames] Du hast erfolgreich das Produkt .+ für (?<orbs>[\\d.]+) Orbs gekauft\\.$");
@@ -61,7 +55,7 @@ public class OrbBalance extends Laby4Module implements TempOrbBalanceBridge {
 	}
 
 	@EventListener(triggerWhenDisabled = true)
-	public void onTick(TickEvent.ClientTickEvent event) {
+	public void onTick(ClientTickEvent event) {
 		if (!ServerCheck.isOnCitybuild() || !(mc().currentScreen instanceof GuiChest))
 			return;
 

090dbcb0d12cba431cb30d89ee6c0dd56f8b940b
L3g7 <contact@l3g7.dev>
2024-08-25 16:08:11 +0200

    Merge Redstone

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/widgets/misc/Redstone.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/widgets/misc/Redstone.java
@@ -5,23 +5,20 @@
  * you may not use this file except in compliance with the License.
  */
 
-package dev.l3g7.griefer_utils.features.modules.laby4;
+package dev.l3g7.griefer_utils.features.widgets.misc;
 
-import dev.l3g7.griefer_utils.core.api.bridges.Bridge.ExclusiveTo;
 import dev.l3g7.griefer_utils.core.api.event_bus.EventListener;
 import dev.l3g7.griefer_utils.core.api.file_provider.Singleton;
-import dev.l3g7.griefer_utils.features.Feature.MainElement;
-import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
 import dev.l3g7.griefer_utils.core.events.network.MysteryModPayloadEvent;
-import dev.l3g7.griefer_utils.features.modules.Laby4Module;
+import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
+import dev.l3g7.griefer_utils.features.Feature.MainElement;
+import dev.l3g7.griefer_utils.features.widgets.SimpleWidget;
 import net.minecraft.init.Items;
 
-import static dev.l3g7.griefer_utils.core.api.bridges.Bridge.Version.LABY_4;
-import static dev.l3g7.griefer_utils.core.util.MinecraftUtil.mc;
+import static dev.l3g7.griefer_utils.core.util.MinecraftUtil.world;
 
 @Singleton
-@ExclusiveTo(LABY_4)
-public class Redstone extends Laby4Module {
+public class Redstone extends SimpleWidget {
 
 	private int redstoneState = -1;
 
@@ -41,7 +38,7 @@ public class Redstone extends Laby4Module {
 
 	@Override
 	public String getValue() {
-		if (mc().theWorld == null)
+		if (world() == null)
 			return "Unbekannt";
 
 		return switch (redstoneState) {
@@ -49,11 +46,6 @@ public class Redstone extends Laby4Module {
 			case 0 -> "§aAktiviert";
 			default -> "§4Deaktiviert";
 		};
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return super.isEnabled();
 	}
 
 }

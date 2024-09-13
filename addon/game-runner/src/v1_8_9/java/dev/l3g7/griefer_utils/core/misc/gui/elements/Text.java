ad8a29f8ad38d85fe81dcc3ffcf0ed257c3360ba
L3g7 <contact@l3g7.dev>
2024-09-13 22:05:05 +0200

    Fix copyright

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/misc/gui/elements/Text.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/misc/gui/elements/Text.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
 package dev.l3g7.griefer_utils.core.misc.gui.elements;
 
 import dev.l3g7.griefer_utils.core.misc.gui.elements.laby_polyfills.DrawUtils;
--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/item/recraft/decompressor/DecompressRecorder.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/item/recraft/decompressor/DecompressRecorder.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
 package dev.l3g7.griefer_utils.features.item.recraft.decompressor;
 
 import dev.l3g7.griefer_utils.core.api.bridges.LabyBridge;

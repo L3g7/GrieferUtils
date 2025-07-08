deaf3b273ea561b794b57199d531c90b5fa29b5f
L3g73 <103503837+L3g73@users.noreply.github.com>
2025-07-08 20:42:13 +0200

    Remove ungenerateable biome

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/world/bsf/data/Category.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/world/bsf/data/Category.java
@@ -17,7 +17,6 @@ import java.util.List;
 public enum Category implements BSFSearchable {
 
 	PLAINS(new BSFName("", "Ebene", "n"), new Color(0x8DB360), Blocks.grass, Biome.PLAINS, Biome.SNOWY_TUNDRA, Biome.SUNFLOWER_PLAINS),
-	ICE(new BSFName("s", "Eis", "Eis-Biome"), new Color(0xB4DCDC), Blocks.packed_ice, Biome.FROZEN_OCEAN, Biome.ICE_SPIKES),
 	BIRCH(new BSFName("n", "Birkenwald", "Birkenwälder"), new Color(0x589C6C), new ItemStack(Blocks.log, 1, 2), Biome.BIRCH_FOREST, Biome.TALL_BIRCH_FOREST),
 	FOREST(new BSFName("n", "Wald", "Wälder"), new Color(0x056621), Blocks.log, Biome.FOREST, Biome.DARK_FOREST, Biome.FLOWER_FOREST, BIRCH),
 	MOUNTAINS(new BSFName("", "Berge", ""), new Color(0x606060), Blocks.stone, Biome.MOUNTAINS, Biome.SNOWY_MOUNTAINS, Biome.GRAVELLY_MOUNTAINS),
@@ -25,7 +24,7 @@ public enum Category implements BSFSearchable {
 	BEACH(new BSFName("n", "Strand", "Strände"), new Color(0xFADE55), Blocks.sand, Biome.BEACH, Biome.SNOWY_BEACH, Biome.STONE_SHORE),
 	SAVANNA(new BSFName("", "Savanne", "n"), new Color(0xBDB25F), Blocks.log2, Biome.SAVANNA, Biome.SHATTERED_SAVANNA),
 	BADLANDS(new BSFName("", "Tafelberge", ""), new Color(0xD94515), Blocks.hardened_clay, Biome.BADLANDS, Biome.ERODED_BADLANDS, Biome.WOODED_BADLANDS),
-	ALL(null, null, null, Biome.OCEAN, Category.PLAINS, Category.ICE, Biome.DESERT, Category.FOREST, Category.MOUNTAINS, Category.TAIGA, Biome.SWAMP, Biome.MUSHROOM_FIELDS, Category.BEACH, Biome.JUNGLE, Category.SAVANNA, Category.BADLANDS),
+	ALL(null, null, null, Biome.OCEAN, Category.PLAINS, Biome.ICE_SPIKES, Biome.DESERT, Category.FOREST, Category.MOUNTAINS, Category.TAIGA, Biome.SWAMP, Biome.MUSHROOM_FIELDS, Category.BEACH, Biome.JUNGLE, Category.SAVANNA, Category.BADLANDS),
 	ALL_STRUCTURES(null, null, null, Structure.values());
 
 	public final BSFSearchable[] entries;

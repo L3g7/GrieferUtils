1b09ee154ec48f9df2b32596111b4d2bae4843f7
L3g73 <103503837+L3g73@users.noreply.github.com>
2025-09-18 14:40:31 +0200

    Fix EventBusProfiler description

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/uncategorized/debug/EventBusProfiler.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/uncategorized/debug/EventBusProfiler.java
@@ -21,7 +21,7 @@ public class EventBusProfiler {
 
 	public static final NumberSetting minTime = NumberSetting.create()
 		.name("Mindestdauer")
-		.description("Wie lange Events mindestens zum Auslösen brauchen, damit sie geloggt werden.")
+		.description("Wie lange Events mindestens zum Auslösen brauchen, damit sie geloggt werden (in ms).")
 		.icon(Blocks.barrier)
 		.min(0)
 		.defaultValue(5);
@@ -33,7 +33,7 @@ public class EventBusProfiler {
 
 	public static final SwitchSetting enabled = SwitchSetting.create()
 		.name("Eventbus-Profiler")
-		.description("Misst, wie das Auslösen von Events / ihrer Listener dauert.")
+		.description("Misst, wie lange das Auslösen von Events / ihrer Listener dauert.")
 		.icon(Items.clock)
 		.callback(EventBusProfiler::updateProfiler)
 		.subSettings(minTime, filter);

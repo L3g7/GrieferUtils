b2806f35ca7104729f566a45029ecd52dbcef94b
L3g7 <contact@l3g7.dev>
2025-09-30 20:50:51 +0200

    Fix LM4 icons

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby4/settings/Icons.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby4/settings/Icons.java
@@ -9,6 +9,7 @@ package dev.l3g7.griefer_utils.labymod.laby4.settings;
 
 import net.labymod.api.Laby;
 import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.client.gui.screen.state.ScreenCanvas;
 import net.labymod.api.client.render.batch.ResourceRenderContext;
 import net.labymod.api.client.render.matrix.Stack;
 import net.labymod.api.client.resources.ResourceLocation;
@@ -17,6 +18,12 @@ import net.minecraft.block.Block;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static dev.l3g7.griefer_utils.core.api.reflection.Reflection.c;
 
@@ -86,10 +93,10 @@ public class Icons {
 
 	}
 
-	private static class OffsetIcon extends Icon {
+	public static class OffsetIcon extends Icon {
 
-		private final float offsetX, offsetY;
-		private final Icon icon;
+		public final float offsetX, offsetY;
+		public final Icon icon;
 
 		public OffsetIcon(Icon icon, float offsetX, float offsetY) {
 			super(null);
@@ -97,15 +104,20 @@ public class Icons {
 			this.offsetX = offsetX;
 			this.offsetY = offsetY;
 		}
+	}
 
-		@Override
-		public void render(Stack stack, float x, float y, float width, float height, boolean hover, int color, Rectangle stencil) {
-			icon.render(stack, x + offsetX, y + offsetY, width, height, hover, color, stencil);
-		}
+	@Mixin(value = ScreenCanvas.class, remap = false)
+	private static abstract class ScreenCanvasMixin {
 
-		@Override
-		public void render(ResourceRenderContext context, float x, float y, float width, float height, boolean hover, int color) {
-			icon.render(context, x + offsetX, y + offsetY, width, height, hover, color);
+		@Shadow
+		public abstract void submitIcon(Icon icon, float x, float y, float width, float height, boolean hover, int argb, @Nullable Rectangle clipBounds);
+
+		@Inject(method = "submitIcon(Lnet/labymod/api/client/gui/icon/Icon;FFFFZILnet/labymod/api/util/bounds/Rectangle;)V", at = @At("HEAD"), cancellable = true, remap = false)
+		public void submitIcon(Icon icon, float x, float y, float width, float height, boolean hover, int argb, Rectangle clipBounds, CallbackInfo ci) {
+			if (icon instanceof OffsetIcon offsetIcon) {
+				submitIcon(offsetIcon.icon, x + offsetIcon.offsetX, y + offsetIcon.offsetY, width, height, hover, argb, clipBounds);
+				ci.cancel();
+			}
 		}
 
 	}

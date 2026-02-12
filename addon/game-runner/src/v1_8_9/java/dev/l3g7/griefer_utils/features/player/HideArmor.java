ae1362dbb18074e2f1e61ab6cefc849065b7ac87
L3g73 <103503837+L3g73@users.noreply.github.com>
2026-02-12 16:32:33 +0100

    Add HideArmor

---
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/player/HideArmor.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
+package dev.l3g7.griefer_utils.features.player;
+
+import dev.l3g7.griefer_utils.core.api.file_provider.FileProvider;
+import dev.l3g7.griefer_utils.core.api.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
+import dev.l3g7.griefer_utils.features.Feature;
+import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Singleton
+public class HideArmor extends Feature {
+
+	private final SwitchSetting helmet = SwitchSetting.create()
+		.name("Helme verstecken")
+		.icon(Items.diamond_helmet)
+		.defaultValue(true);
+	private final SwitchSetting chestplate = SwitchSetting.create()
+		.name("Brustpanzer verstecken")
+		.icon(Items.diamond_chestplate)
+		.defaultValue(true);
+	private final SwitchSetting leggings = SwitchSetting.create()
+		.name("Hosen verstecken")
+		.icon(Items.diamond_leggings)
+		.defaultValue(true);
+	private final SwitchSetting boots = SwitchSetting.create()
+		.name("Schuhe verstecken")
+		.icon(Items.diamond_boots)
+		.defaultValue(true);
+
+	private final SwitchSetting[] subsettings = new SwitchSetting[] { helmet, chestplate, leggings, boots };
+
+	@MainElement
+	private final SwitchSetting enabled = SwitchSetting.create()
+		.name("Rüstung verstecken")
+		.description("Versteckt angezogene Rüstungen von Spielern.")
+		.icon(Items.diamond_chestplate)
+		.subSettings(subsettings);
+
+	public static boolean shouldRender(int index) {
+		if (index < 1 || index > 4)
+			return true;
+
+		HideArmor hideArmor = FileProvider.getSingleton(HideArmor.class);
+		if (!hideArmor.isEnabled())
+			return true;
+
+		return !hideArmor.subsettings[4 - index].get();
+	}
+
+	@Mixin(LayerArmorBase.class)
+	private static class MixinLayerArmorBase {
+
+		@Inject(method = "renderLayer", at = @At("HEAD"), cancellable = true)
+		private void injectRenderLayer(EntityLivingBase lvt_1_1_, float lvt_2_1_, float lvt_3_1_, float lvt_4_1_, float lvt_5_1_, float lvt_6_1_, float lvt_7_1_, float lvt_8_1_, int lvt_9_1_, CallbackInfo ci) {
+			if (!shouldRender(lvt_9_1_))
+				ci.cancel();
+		}
+
+	}
+
+}

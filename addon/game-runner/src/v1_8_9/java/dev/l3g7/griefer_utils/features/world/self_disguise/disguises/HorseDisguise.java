c4d82a0bff647180d702ba9a85b67d3f43fca64a
L3g7 <contact@l3g7.dev>
2026-02-20 00:05:37 +0100

    Remove armor for skeletal and dead horses

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/world/self_disguise/disguises/HorseDisguise.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/world/self_disguise/disguises/HorseDisguise.java
@@ -1,6 +1,8 @@
 package dev.l3g7.griefer_utils.features.world.self_disguise.disguises;
 
 import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 /**
  * Disguised as {@link EntityHorse} (base type).
@@ -16,6 +18,7 @@ public class HorseDisguise extends AbstractHorseDisguise {
 
 	@Override
 	public EntityHorse create(Arguments arguments) {
+		String armor = arguments.getEnum("diamond", "gold", "iron");
 		String color = arguments.getEnum("brown", colors);
 		String colorVariant = arguments.getEnum(null, variants);
 
@@ -25,6 +28,12 @@ public class HorseDisguise extends AbstractHorseDisguise {
 
 		EntityHorse entity = super.create(arguments);
 		entity.setHorseVariant(variant);
+		entity.setHorseArmorStack(switch (armor) {
+			case "diamond" -> new ItemStack(Items.diamond_horse_armor);
+			case "gold" -> new ItemStack(Items.golden_horse_armor);
+			case "iron" -> new ItemStack(Items.iron_horse_armor);
+			case null, default -> null;
+		});
 
 		return entity;
 	}

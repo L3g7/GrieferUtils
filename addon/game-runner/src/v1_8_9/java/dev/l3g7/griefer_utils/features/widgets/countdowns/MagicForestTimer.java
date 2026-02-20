382004465b18291a151f80ae5f4d30ef720219cf
L3g73 <103503837+L3g73@users.noreply.github.com>
2026-02-20 17:22:36 +0100

    Add MagicForestTimer

---
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/widgets/countdowns/MagicForestTimer.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
+package dev.l3g7.griefer_utils.features.widgets.countdowns;
+
+import dev.l3g7.griefer_utils.core.api.event_bus.EventListener;
+import dev.l3g7.griefer_utils.core.api.file_provider.Singleton;
+import dev.l3g7.griefer_utils.core.api.misc.Citybuild;
+import dev.l3g7.griefer_utils.core.events.MessageEvent.MessageReceiveEvent;
+import dev.l3g7.griefer_utils.core.events.griefergames.CitybuildJoinEvent;
+import dev.l3g7.griefer_utils.core.misc.Countdown;
+import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
+import dev.l3g7.griefer_utils.core.util.MinecraftUtil;
+import dev.l3g7.griefer_utils.features.Feature.MainElement;
+import dev.l3g7.griefer_utils.features.widgets.Widget.SimpleWidget;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Singleton
+public class MagicForestTimer extends SimpleWidget {
+
+	private static final Pattern TIME_PATTERN = Pattern.compile("^§r§8\\[§r§6GrieferGames§r§8] §r§7Du hast noch §r§e(?<hours>\\d+) §r§7Stunden? §r§e(?<minutes>\\d+) §r§7Minuten? §r§e(?<seconds>\\d+) §r§7Sekunden? Zeit im Zauberwald zu sein\\.§r$");
+	private static boolean waitingForTime = false;
+
+	private static final Countdown COUNTDOWN = Countdown.realtime();
+
+	@MainElement
+	private final SwitchSetting enabled = SwitchSetting.create()
+		.name("Zauberwald-\nTimer")
+		.description("Zeigt dir an, wie viel Zeit du noch im Zauberwald hast.")
+		.icon("hourglass");
+
+	@Override
+	public boolean isVisibleInGame() {
+		return MinecraftUtil.getCurrentCitybuild() == Citybuild.MAGIC_FOREST;
+	}
+
+	@Override
+	public String getValue() {
+		int secondsRemaining = Math.max(COUNTDOWN.secondsRemaining(), 0);
+
+		int seconds = secondsRemaining % 60;
+		int minutes = (secondsRemaining / 60) % 60;
+		int hours = secondsRemaining / 3600;
+		return String.format("%2s:%2s:%2s", hours, minutes, seconds).replace(' ', '0');
+	}
+
+	@EventListener
+	private void onJoin(CitybuildJoinEvent.Early event) {
+		if (event.citybuild != Citybuild.MAGIC_FOREST)
+			return;
+
+		MinecraftUtil.send("/zauberwald");
+		waitingForTime = true;
+	}
+
+	@EventListener
+	private void onMessageReceive(MessageReceiveEvent event) {
+		if (!waitingForTime)
+			return;
+
+		Matcher matcher = TIME_PATTERN.matcher(event.message.getFormattedText());
+		if (!matcher.matches())
+			return;
+
+		waitingForTime = false;
+		int hours = Integer.parseInt(matcher.group("hours"));
+		int minutes = Integer.parseInt(matcher.group("minutes"));
+		int seconds = Integer.parseInt(matcher.group("seconds"));
+		COUNTDOWN.set((hours * 60 + minutes) * 60 + seconds);
+		event.cancel();
+	}
+
+}

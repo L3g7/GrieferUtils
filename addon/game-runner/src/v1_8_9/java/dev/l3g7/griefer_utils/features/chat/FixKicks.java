5ed59f5ddbfe915adec4ea3713d017177fde707f
L3g7 <contact@l3g7.dev>
2026-02-26 18:39:52 +0100

    Fix typo

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/chat/FixKicks.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/chat/FixKicks.java
@@ -17,8 +17,8 @@ public class FixKicks extends Feature {
 	@MainElement
 	private final SwitchSetting enabled = SwitchSetting.create()
 		.name("Kicks verhindern")
-		.description("Verhindert Kicks aufgrund ungültiger Zeichen in gesendet Nachrichten.")
 		.icon("crossed_out_chat");
+		.description("Verhindert Kicks aufgrund ungültiger Zeichen in gesendeten Nachrichten.")
 
 	@EventListener(priority = LOW)
 	private void onMessageSend(MessageEvent.MessageSendEvent event) {

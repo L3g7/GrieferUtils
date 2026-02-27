6ae060c2bdffb84b1bcf8ddc1508b03d62dd1bc4
L3g73 <103503837+L3g73@users.noreply.github.com>
2026-02-27 10:29:01 +0100

    Fix FixKicks not working in LabyMod 3

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/chat/FixKicks.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/features/chat/FixKicks.java
@@ -3,13 +3,13 @@ package dev.l3g7.griefer_utils.features.chat;
 import dev.l3g7.griefer_utils.core.api.event_bus.EventListener;
 import dev.l3g7.griefer_utils.core.api.file_provider.Singleton;
 import dev.l3g7.griefer_utils.core.api.misc.Constants;
-import dev.l3g7.griefer_utils.core.events.MessageEvent;
+import dev.l3g7.griefer_utils.core.events.MessageEvent.MessageAboutToBeSentEvent;
 import dev.l3g7.griefer_utils.core.settings.types.SwitchSetting;
 import dev.l3g7.griefer_utils.features.Feature;
 import net.minecraft.util.ChatAllowedCharacters;
 
 import static dev.l3g7.griefer_utils.core.api.bridges.LabyBridge.display;
-import static dev.l3g7.griefer_utils.core.api.event_bus.Priority.LOW;
+import static dev.l3g7.griefer_utils.core.api.event_bus.Priority.HIGH;
 
 @Singleton
 public class FixKicks extends Feature {
@@ -20,8 +20,8 @@ public class FixKicks extends Feature {
 		.description("Verhindert Kicks aufgrund ungültiger Zeichen in gesendeten Nachrichten.")
 		.icon("chat_checkmark");
 
-	@EventListener(priority = LOW)
-	private void onMessageSend(MessageEvent.MessageSendEvent event) {
+	@EventListener(priority = HIGH)
+	private void onMessageSend(MessageAboutToBeSentEvent event) {
 		if (event.message.chars().anyMatch(c -> !ChatAllowedCharacters.isAllowedCharacter((char) c))) {
 			event.cancel();
 			display(Constants.ADDON_PREFIX + "§cUngültiges Zeichen in \"" + event.message + "§r§c\"");

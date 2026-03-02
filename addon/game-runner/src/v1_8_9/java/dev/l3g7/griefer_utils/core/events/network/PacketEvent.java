e55297fa1b9ade38f6719a29308183b5f165e8c3
L3g73 <103503837+L3g73@users.noreply.github.com>
2026-03-02 22:17:45 +0100

    Fix PacketReceivedEvent only firing in the network thread

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/events/network/PacketEvent.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/events/network/PacketEvent.java
@@ -10,8 +10,11 @@ package dev.l3g7.griefer_utils.core.events.network;
 import dev.l3g7.griefer_utils.core.api.event_bus.Event;
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
+import net.minecraft.network.PacketThreadUtil;
+import net.minecraft.util.IThreadListener;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -54,6 +57,9 @@ public abstract class PacketEvent<P extends Packet<?>> extends Event {
 	 * Fired after a packet was processed.
 	 */
 	public static class PacketReceivedEvent<P extends Packet<?>> extends PacketEvent<P> {
+
+		public static Packet<?> LAST_QUEUED_PACKET;
+
 		public PacketReceivedEvent(P packet, NetworkManager manager) {
 			super(packet, manager);
 		}
@@ -61,9 +67,29 @@ public abstract class PacketEvent<P extends Packet<?>> extends Event {
 		@Mixin(NetworkManager.class)
 		private static class MixinNetworkManager {
 
-			@Inject(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/Packet;)V", at = @At("TAIL"))
+			@Inject(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/Packet;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V", shift = At.Shift.AFTER))
 			public void injectChannelRead0(ChannelHandlerContext ctx, Packet<?> packet, CallbackInfo ci) {
 				new PacketReceivedEvent<>(packet, c(this)).fire();
+			}
+
+		}
+
+		@Mixin(PacketThreadUtil.class)
+		private static class MixinPacketThreadUtil {
+
+			@Inject(method = "checkThreadAndEnqueue", at = @At("HEAD"))
+			private static <T extends INetHandler> void onCheckThreadAndEnqueue(Packet<T> lvt_0_1_, T lvt_1_1_, IThreadListener lvt_2_1_, CallbackInfo ci) {
+				LAST_QUEUED_PACKET = lvt_0_1_;
+			}
+
+		}
+
+		@Mixin(targets = "net.minecraft.network.PacketThreadUtil$1")
+		private static class MixinPacketThreadUtil$1 {
+
+			@Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V", shift = At.Shift.AFTER))
+			private void injectCheckThreadAndEnqueue(CallbackInfo ci) {
+				new PacketReceivedEvent<>(LAST_QUEUED_PACKET, null).fire();
 			}
 
 		}

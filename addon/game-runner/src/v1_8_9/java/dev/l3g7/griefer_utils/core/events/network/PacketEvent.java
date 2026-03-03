2f4caf858787cd932e2d04fb046672d35246bf13
L3g73 <103503837+L3g73@users.noreply.github.com>
2026-03-03 08:30:28 +0100

    Capture synthetic fields to fire PacketReceivedEvent

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/events/network/PacketEvent.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/core/events/network/PacketEvent.java
@@ -11,11 +11,8 @@ import dev.l3g7.griefer_utils.core.api.event_bus.Event;
 import dev.l3g7.griefer_utils.core.injection.InheritedInvoke;
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
-import net.minecraft.network.PacketThreadUtil;
-import net.minecraft.util.IThreadListener;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -59,8 +56,6 @@ public abstract class PacketEvent<P extends Packet<?>> extends Event {
 	 */
 	public static class PacketReceivedEvent<P extends Packet<?>> extends PacketEvent<P> {
 
-		public static Packet<?> LAST_QUEUED_PACKET;
-
 		public PacketReceivedEvent(P packet, NetworkManager manager) {
 			super(packet, manager);
 		}
@@ -75,23 +70,16 @@ public abstract class PacketEvent<P extends Packet<?>> extends Event {
 
 		}
 
-		@Mixin(PacketThreadUtil.class)
-		private static class MixinPacketThreadUtil {
-
-			@Inject(method = "checkThreadAndEnqueue", at = @At("HEAD"))
-			private static <T extends INetHandler> void onCheckThreadAndEnqueue(Packet<T> lvt_0_1_, T lvt_1_1_, IThreadListener lvt_2_1_, CallbackInfo ci) {
-				LAST_QUEUED_PACKET = lvt_0_1_;
-			}
-
-		}
-
 		@Mixin(targets = "net.minecraft.network.PacketThreadUtil$1")
 		private static class MixinPacketThreadUtil$1 {
+
+			@Shadow(aliases = "field_180030_a")
+			private Packet<?> packet;
 
 			@InheritedInvoke(Runnable.class)
 			@Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V", shift = At.Shift.AFTER))
 			private void injectCheckThreadAndEnqueue(CallbackInfo ci) {
-				new PacketReceivedEvent<>(LAST_QUEUED_PACKET, null).fire();
+				new PacketReceivedEvent<>(packet, null).fire();
 			}
 
 		}

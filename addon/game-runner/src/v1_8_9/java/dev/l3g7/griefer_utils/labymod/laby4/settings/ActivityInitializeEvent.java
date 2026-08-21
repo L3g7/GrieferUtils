f579795a95464f7cef06579c9778c8d939d82351
L3g7 <contact@l3g7.dev>
2026-08-21 22:01:15 +0200

    Fix copyright header

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby4/settings/ActivityInitializeEvent.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby4/settings/ActivityInitializeEvent.java
@@ -1,5 +1,11 @@
-package dev.l3g7.griefer_utils.labymod.laby4.settings;
+/*
+ * This file is part of GrieferUtils (https://github.com/L3g7/GrieferUtils).
+ * Copyright (c) L3g7.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
 
+package dev.l3g7.griefer_utils.labymod.laby4.settings;
 
 import dev.l3g7.griefer_utils.core.api.bridges.Bridge.ExclusiveTo;
 import dev.l3g7.griefer_utils.core.api.event_bus.Event;

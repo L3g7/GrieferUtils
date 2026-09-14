9d49646b5cc0cdbfde764d7791c853dcafb888c0
L3g7 <contact@l3g7.dev>
2026-09-14 15:22:18 +0200

    Update Gradle to 9.7.1

--- addon/settings.gradle.kts
+++ addon/settings.gradle.kts
@@ -1,21 +1,19 @@
 rootProject.name = "GrieferUtils"
 
 pluginManagement {
-	val labyGradlePluginVersion = "0.8.1"
-	buildscript {
-        repositories {
-	        maven("https://maven.laby.net/api/v1/maven/release/")
-			maven("https://jitpack.io/")
-			maven("https://maven.neoforged.net/releases/")
-			maven("https://maven.fabricmc.net/")
-			gradlePluginPortal()
-			mavenCentral()
-        }
+    repositories {
+        maven("https://maven.laby.net/api/v1/maven/release/")
+        maven("https://maven.neoforged.net/releases/")
+        maven("https://maven.fabricmc.net/")
+        gradlePluginPortal()
+        mavenCentral()
+    }
 
-        dependencies {
-            classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
-        }
+    plugins {
+        id("net.labymod.labygradle.settings") version "0.8.1"
     }
 }
 
-plugins.apply("net.labymod.labygradle.settings")
+plugins {
+    id("net.labymod.labygradle.settings")
+}
